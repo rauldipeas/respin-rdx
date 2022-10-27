6039be3424dbce1878b9cd80e69b405ecfcda184
Raul Dipeas <rauldipeas@disroot.org>
2022-10-27 10:23:50 -0300

    primeiro commit

---
+++ scripts/config.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# This script provides common customization options for the ISO
+# 
+# Usage: Copy this file to config.sh and make changes there.  Keep this file (default_config.sh) as-is
+#   so that subsequent changes can be easily merged from upstream.  Keep all customiations in config.sh
+
+# The version of Ubuntu to generate.  Successfully tested: bionic, cosmic, disco, eoan, focal, groovy, jammy
+# See https://wiki.ubuntu.com/DevelopmentCodeNames for details
+export TARGET_UBUNTU_VERSION="jammy"
+
+# The Ubuntu Mirror URL. It's better to change for faster download.
+# More mirrors see: https://launchpad.net/ubuntu/+archivemirrors
+export TARGET_UBUNTU_MIRROR="http://br.archive.ubuntu.com/ubuntu/"
+
+# The packaged version of the Linux kernel to install on target image.
+# See https://wiki.ubuntu.com/Kernel/LTSEnablementStack for details
+export TARGET_KERNEL_PACKAGE="linux-lowlatency"
+
+# The file (no extension) of the ISO containing the generated disk image,
+# the volume id, and the hostname of the live environment are set from this name.
+export TARGET_NAME="kubuntu-from-scratch"
+
+# The text label shown in GRUB for booting into the live environment
+export GRUB_LIVEBOOT_LABEL="Testar o Kubuntu FS sem instalar"
+
+# The text label shown in GRUB for starting installation
+export GRUB_INSTALL_LABEL="Instalar o Kubuntu FS"
+
+# Packages to be removed from the target system after installation completes succesfully
+export TARGET_PACKAGE_REMOVE="
+    ubiquity \
+    casper \
+    discover \
+    laptop-detect \
+    os-prober \
+"
+
+# Package customisation function.  Update this function to customize packages
+# present on the installed system.
+function customize_image() {
+    # install graphics and desktop
+    apt install -y kubuntu-desktop
+
+    # useful tools
+    apt install -y \
+    python3-pip \
+    python3-tk \
+    ubuntustudio-lowlatency-settings \
+    ubuntustudio-performance-settings
+
+    # purge
+#    apt purge -y \
+#    transmission-gtk \
+#    transmission-common \
+#    gnome-mahjongg \
+#    gnome-mines \
+#    gnome-sudoku \
+#    aisleriot \
+#    hitori
+
+	# rtcqs
+	pip install -q rtcqs
+	wget -qO /usr/share/applications/rtcqs.desktop https://github.com/autostatic/rtcqs/raw/main/rtcqs.desktop
+	wget -qO /usr/share/icons/rtcqs.svg https://github.com/autostatic/rtcqs/raw/main/rtcqs_logo.svg
+
+	# GRUB
+	cat <<EOF |tee /etc/default/grub.d/cmdline-linux-default.cfg >/dev/null
+	GRUB_CMDLINE_LINUX_DEFAULT="cpufreq.default_governor=performance mitigations=off preempt=full quiet splash threadirqs"
+	EOF
+
+	# CPU DMA latency
+	wget -qO /etc/udev/rules.d/99-cpu-dma-latency.rules https://raw.githubusercontent.com/Ardour/ardour/master/tools/udev/99-cpu-dma-latency.rules
+}
+
+# Used to version the configuration.  If breaking changes occur, manual
+# updates to this file from the default may be necessary.
+export CONFIG_FILE_VERSION="0.4"
