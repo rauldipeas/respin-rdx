f9ec5a3281643d40848304ded8e2ca8c616f1c77
Raul Dipeas <rauldipeas@disroot.org>
2022-10-29 12:25:36 -0300

    ^scripts

--- scripts/chroot_build.sh
+++ scripts/chroot_build.sh
@@ -145,21 +145,21 @@ function install_pkg() {
     apt autoremove -y
 
     # final touch
-    dpkg-reconfigure locales
-    dpkg-reconfigure resolvconf
+#    dpkg-reconfigure locales
+#    dpkg-reconfigure resolvconf
 
     # network manager
-    cat <<EOF > /etc/NetworkManager/NetworkManager.conf
-[main]
-rc-manager=resolvconf
-plugins=ifupdown,keyfile
-dns=dnsmasq
+#    cat <<EOF > /etc/NetworkManager/NetworkManager.conf
+#[main]
+#rc-manager=resolvconf
+#plugins=ifupdown,keyfile
+#dns=dnsmasq
+#
+#[ifupdown]
+#managed=false
+#EOF
 
-[ifupdown]
-managed=false
-EOF
-
-    dpkg-reconfigure network-manager
+#    dpkg-reconfigure network-manager
 
     apt clean -y
 }
