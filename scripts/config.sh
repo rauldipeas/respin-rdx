3f0c4e128e3552e13da6832b38da20fc0e79d420
Raul Dipeas <rauldipeas@disroot.org>
2022-10-30 00:08:50 -0300

    ^scripts

--- scripts/config.sh
+++ scripts/config.sh
@@ -176,10 +176,10 @@ EOF
     apt install -y heroic
 
     # Steam
-    dpkg --add-architecture i386
+    #dpkg --add-architecture i386
     #add-apt-repository -ny multiverse
     #add-apt-repository -y universe
-    apt install -y steam-installer
+    #apt install -y steam-installer
 
 }
 
