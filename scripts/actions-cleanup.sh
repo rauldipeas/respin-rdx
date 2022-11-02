9a7fd1ed2e3f56c0d37f811a8ff053c7b66808b4
Raul Dipeas <rauldipeas@disroot.org>
2022-11-02 20:39:26 +0000

    ^respin-rdx

--- scripts/actions-cleanup.sh
+++ scripts/actions-cleanup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 # Actions cleanup
-PROJECT='rauldipeas/kubuntu-rdx'
-KEEP=2
+PROJECT='rauldipeas/respin-rdx'
+KEEP=1
 gh api\
 	repos/"$PROJECT"/actions/runs\
 	--paginate -q '.workflow_runs[]|select(.head_branch != "any")|"\(.id)"'|tail -n+$((KEEP+1))\
