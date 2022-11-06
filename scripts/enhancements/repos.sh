64d7f68c85d6cc6fedfddd74418b605cfd38bd0e
Raul Dipeas <rauldipeas@disroot.org>
2022-11-06 23:24:51 +0000

    ^scripts

--- scripts/enhancements/repos.sh
+++ scripts/enhancements/repos.sh
@@ -9,6 +9,7 @@ add-apt-repository -ny universe
 bash -x <(wget -qO- https://raw.githubusercontent.com/rauldipeas/apt-repository/main/apt-repository.sh)
 
 # deb-get
+apt update
 wget -q "$(wget -qO- https://api.github.com/repos/wimpysworld/deb-get/releases|grep browser_download_url|grep download|grep .deb|head -n1|cut -d '"' -f4)"
 sudo apt install -y ./deb-get*.deb
 rm deb-get*.deb
