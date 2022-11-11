75146ed03b1efaa552b90adc0c06fab36907dc36
Raul Dipeas <rauldipeas@disroot.org>
2022-11-11 20:10:09 -0300

    +ugl-respin-rdx ^respin-rdx ^scripts

--- scripts/enhancements/desktop.sh
+++ scripts/enhancements/desktop.sh
@@ -84,3 +84,35 @@ if [ -f /usr/share/xsessions/xubuntu.desktop ]; then
     sed -i 's/Greybird/Greybird-dark/g' /etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
     sed -i 's/elementary-xfce-darker/Papirus-Dark/g' /etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
 fi
+## GNOME Lite
+if [ $FLAVOUR_NAME = Ubuntu\ GNOME\ Lite ]; then
+    echo 'GNOME Lite'
+    sudo papirus-folders -C adwaita
+    #sed -i 's/firefox_firefox/firefox/g' /usr/share/glib-2.0/schemas/10_ubuntu-settings.gschema.override
+    #sed -i 's/gtk-theme = "Yaru"/gtk-theme = "Yaru-dark"/g' /usr/share/glib-2.0/schemas/10_ubuntu-settings.gschema.override
+    #sed -i 's/icon-theme = "Yaru"/icon-theme = "Papirus-Dark"/g' /usr/share/glib-2.0/schemas/10_ubuntu-settings.gschema.override
+    #sed -i 's/snap-store_ubuntu-software/synaptic/g' /usr/share/glib-2.0/schemas/10_ubuntu-settings.gschema.override
+    #sed -i 's/warty-final-ubuntu.png/DSC2943_by_kcpru.jpg/g' /usr/share/glib-2.0/schemas/10_ubuntu-settings.gschema.override
+    apt autoreremove --purge -y\
+        ubuntu-desktop*\
+        ubuntu-session*
+        #gdm3*\
+        #gnome-*\
+    apt install -y\
+        gnome-session
+        #--no-install-recommends -y\
+        #gnome-shell\
+        #gnome-icon-theme\
+        #gnome-icon-theme-symbolic\
+        #nautilus\
+        #gnome-terminal\
+        #gnome-control-center\
+        #gnome-online-accounts\
+        #network-manager-gnome\
+        #gdm3\
+        #xinit\
+        #gnome-tweaks\
+        #yaru-theme-icon\
+        #yaru-theme-gtk\
+        #yaru-theme-shell
+fi
