c28c14b5b785f2f7f10c6984b8fb4422b77a4a21
Raul Dipeas <rauldipeas@disroot.org>
2022-11-11 02:29:49 +0000

    ^scripts

--- scripts/enhancements/kernel.sh
+++ scripts/enhancements/kernel.sh
@@ -17,11 +17,13 @@ add-apt-repository -y ppa:cappelikan/ppa
 apt install -y amd64-microcode intel-microcode mainline
 sed -i 's/Icon=mainline/Icon=mintsources-ppa/g' /usr/share/applications/mainline.desktop
 
-# Latest kernel version
+# Latest kernel
 KERNEL_VER=$(wget -qO- https://kernel.ubuntu.com/~kernel-ppa/mainline/|grep folder|tail -n1|cut -d '"' -f8)
+KERNEL_HEADERS_COMMON_FILE=$(wget -qO- https://kernel.ubuntu.com/~kernel-ppa/mainline/$KERNEL_VER|grep headers|grep all.deb|cut -d '"' -f2)
 KERNEL_HEADERS_FILE=$(wget -qO- https://kernel.ubuntu.com/~kernel-ppa/mainline/$KERNEL_VER|grep headers|grep amd64.deb|cut -d '"' -f2)
 KERNEL_IMAGE_FILE=$(wget -qO- https://kernel.ubuntu.com/~kernel-ppa/mainline/$KERNEL_VER|grep image|grep amd64.deb|cut -d '"' -f2)
 KERNEL_MODULES_FILE=$(wget -qO- https://kernel.ubuntu.com/~kernel-ppa/mainline/$KERNEL_VER|grep modules|grep amd64.deb|cut -d '"' -f2)
+wget -q https://kernel.ubuntu.com/~kernel-ppa/mainline/$KERNEL_VER$KERNEL_HEADERS_COMMON_FILE
 wget -q https://kernel.ubuntu.com/~kernel-ppa/mainline/$KERNEL_VER$KERNEL_HEADERS_FILE
 wget -q https://kernel.ubuntu.com/~kernel-ppa/mainline/$KERNEL_VER$KERNEL_IMAGE_FILE
 wget -q https://kernel.ubuntu.com/~kernel-ppa/mainline/$KERNEL_VER$KERNEL_MODULES_FILE
