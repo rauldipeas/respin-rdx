af20712ef0e2bcadf014dc147f4fabe3b969a214
Raul Dipeas <rauldipeas@disroot.org>
2022-11-14 12:16:47 +0000

    ^scripts

--- scripts/iso-download.sh
+++ scripts/iso-download.sh
@@ -5,7 +5,7 @@ set -e
 if [ $FLAVOUR = ubuntu ]; then
    wget -q https://releases.ubuntu.com/$VERSION/ubuntu-$VERSION-desktop-amd64.iso
    elif [ $FLAVOUR = neon ]; then
-   https://files.kde.org/neon/images/user/current/neon-user-current.iso
+   wget -q https://files.kde.org/neon/images/user/current/neon-user-current.iso
    else
    wget -q https://cdimage.ubuntu.com/$FLAVOUR/releases/$CODENAME/release/$FLAVOUR-$VERSION-desktop-amd64.iso
 fi
