830e2785a23e694f5bb0a7561dfcd148ce6d3481
Raul Dipeas <rauldipeas@disroot.org>
2022-11-15 18:15:22 +0000

    ^scripts

--- scripts/ia-upload.sh
+++ scripts/ia-upload.sh
@@ -5,4 +5,4 @@ set -e
 wget -q --show-progress https://archive.org/download/ia-pex/ia
 chmod +x ./ia
 cat assets/internetarchive/ia.ini|tee ./ia.ini
-./ia -c ./ia.ini upload "$ISO_NAME"-respin-rdx "$ISO_NAME"-respin-rdx.iso
+./ia -c ./ia.ini upload "$ISO_NAME"-respin-rdx /home/runner/work/respin-rdx/"$ISO_NAME"-respin-rdx.iso
