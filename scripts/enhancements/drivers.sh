95d89eb87a8764b82e60a2e323355a0b0a2fef0a
Raul Dipeas <rauldipeas@disroot.org>
2022-11-17 02:07:00 +0000

    ^readme ^scripts

--- scripts/enhancements/drivers.sh
+++ scripts/enhancements/drivers.sh
@@ -9,3 +9,10 @@ apt dist-upgrade -y
 add-apt-repository -y ppa:ubuntustudio-ppa/backports
 echo 'jackd2 jackd/tweak_rt_limits string true'|debconf-set-selections>/dev/null
 apt install --no-install-recommends -y jackd2
+
+# udev-rtirq
+git clone -q https://github.com/jhernberg/udev-rtirq
+cd udev-rtirq
+make install
+cd ..
+rm -r udev-rtirq
