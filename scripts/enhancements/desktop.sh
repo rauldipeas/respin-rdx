b221d87f89a4d095072437e51c2d697a4c01d8e1
Raul Dipeas <rauldipeas@disroot.org>
2022-11-22 08:31:09 -0300

    ^scripts

--- scripts/enhancements/desktop.sh
+++ scripts/enhancements/desktop.sh
@@ -196,7 +196,7 @@ if [ -f /UCL ]; then
     sed -i '/cursor-theme/{n;s/Adwaita/DMZ-White/;}' /usr/share/glib-2.0/schemas/org.cinnamon.desktop.interface.gschema.xml
     sed -i '/gtk-theme/{n;s/Adwaita/Materia-dark-compact/;}' /usr/share/glib-2.0/schemas/org.cinnamon.desktop.interface.gschema.xml
     sed -i '/icon-theme/{n;s/Adwaita/Papirus-Dark/;}' /usr/share/glib-2.0/schemas/org.cinnamon.desktop.interface.gschema.xml
-    sed -i '@org/cinnamon/theme@{n;n;n;s@>""<@>"Materia-dark-compact"<@;}' /usr/share/glib-2.0/schemas/org.cinnamon.gschema.xml
+    sed -i '/org\/cinnamon\/theme/{n;n;n;s/>""</>"Materia-dark-compact"</;}' /usr/share/glib-2.0/schemas/org.cinnamon.gschema.xml
     sed -i '/picture-uri/{n;s@themes/Adwaita/backgrounds/adwaita-timed.xml@backgrounds/DSC2943_by_kcpru.jpg@;}' /usr/share/glib-2.0/schemas/org.cinnamon.desktop.background.gschema.xml
     sed -i '/tap-to-click/{n;s/false/true/;}' /usr/share/glib-2.0/schemas/org.cinnamon.desktop.peripherals.gschema.xml
     sed -i '/tap-to-click/{n;s/false/true/;}' /usr/share/glib-2.0/schemas/org.cinnamon.settings-daemon.peripherals.gschema.xml
