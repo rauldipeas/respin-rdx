bc15ef007c3c20dc63f2e35048bcec17b94708f9
Raul Dipeas <rauldipeas@disroot.org>
2022-11-30 12:46:52 -0300

    ^scripts

--- scripts/enhancements/kdeck.sh
+++ scripts/enhancements/kdeck.sh
@@ -20,6 +20,7 @@ apt autoremove --purge -y\
 dpkg --add-architecture i386
 sudo add-apt-repository -y ppa:libretro/stable
 wget -q --show-progress -O emulationstation-de_x64.deb $(wget -qO- https://es-de.org|grep x64.deb|cut -d '"' -f8)
+wget -q --show-progress https://cdn.akamai.steamstatic.com/client/installer/steam.deb
 apt install -y\
     boilr\
     ./emulationstation-de_x64.deb\
@@ -27,7 +28,7 @@ apt install -y\
     heroic\
     libssl1.1\
     retroarch\
-    steam-installer\
+    ./steam.deb\
     steamos-compositor-plus
 wget -q "$(wget -qO- https://api.github.com/repos/flightlessmango/MangoHud/releases|grep browser_download_url|grep download/v|grep tar.gz|head -n1|cut -d '"' -f4)"
 tar fxz MangoHud*.tar.gz
