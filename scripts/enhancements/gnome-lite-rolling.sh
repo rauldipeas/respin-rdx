96701dbf13a0ab6e35050d928f540e907570077a
Raul Dipeas <rauldipeas@disroot.org>
2022-12-13 03:44:27 -0300

    ^scripts

--- scripts/enhancements/gnome-lite-rolling.sh
+++ scripts/enhancements/gnome-lite-rolling.sh
@@ -43,4 +43,5 @@ rm -r Adwaita-for-Steam
 wget -q "$(wget -qO- https://api.github.com/repos/flightlessmango/MangoHud/releases|grep browser_download_url|grep download/v|grep tar.gz|head -n1|cut -d '"' -f4)"
 tar fxz MangoHud*.tar.gz
 cd MangoHud
+./mangohud-setup.sh install
 apt autoremove --purge -y yelp*
