9a71c8020fd555038571140f56858ee485b3a9e3
Raul Dipeas <rdipeas+github@proton.me>
2023-02-16 15:57:28 -0300

    ^scripts

--- scripts/enhancements/kdeck.sh
+++ scripts/enhancements/kdeck.sh
@@ -33,3 +33,4 @@ wget -q --show-progress "$(wget -qO- --header="X-Auth-Token: $GITHUB_TOKEN" http
 tar fxz MangoHud*.tar.gz
 cd MangoHud
 ./mangohud-setup.sh install
+pacstall -I -P steamtinkerlaunch-git
