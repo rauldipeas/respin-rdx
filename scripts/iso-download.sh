1e2e6cf5ec467f2211d85dca2b7316a290ed2e37
Raul Dipeas <rdipeas+github@proton.me>
2023-03-12 22:11:43 -0300

    ^respin-rdx ^scripts

--- scripts/iso-download.sh
+++ scripts/iso-download.sh
@@ -3,9 +3,9 @@
 # ISO download
 set -e
 if [ "$FLAVOUR" = ubuntu ]; then
-   wget -q --show-progress https://releases.ubuntu.com/"$VERSION"/ubuntu-"$VERSION"-desktop-amd64.iso
+   wget -q https://releases.ubuntu.com/"$VERSION"/ubuntu-"$VERSION"-desktop-amd64.iso
    elif [ "$FLAVOUR" = neon ]; then
-   wget -q --show-progress https://files.kde.org/neon/images/user/current/neon-user-current.iso
+   wget -q https://files.kde.org/neon/images/user/current/neon-user-current.iso
    else
-   wget -q --show-progress https://cdimage.ubuntu.com/"$FLAVOUR"/releases/"$CODENAME"/release/"$FLAVOUR"-"$VERSION"-desktop-amd64.iso
+   wget -q https://cdimage.ubuntu.com/"$FLAVOUR"/releases/"$CODENAME"/release/"$FLAVOUR"-"$VERSION"-desktop-amd64.iso
 fi
