a7850c9790a7001e9951900d28d6e94b09874602
Raul Dipeas <rdipeas+github@proton.me>
2023-05-20 04:20:32 -0300

    ^readme

--- upgrade-respin-rdx.sh
+++ upgrade-respin-rdx.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 set -e
+# deb-get
 #if [ -f /usr/bin/deb-get ];then
 #        deb-get update
 #        deb-get upgrade
 #        else
 #        echo 'deb-get não encontrado'
 #fi
+# nala
 if [ -f /usr/bin/nala ];then
         sudo nala upgrade
         else
         echo 'nala não encontrado'
 fi
+# pacstall
 if [ -f /usr/bin/pacstall ];then
         pacstall -Up
         else
         echo 'pacstall não encontrado'
 fi
+# flatpak
+if [ -f /usr/bin/flatpak ];then
+        flatpak update
+        else
+        echo 'flatpak não encontrado'
+fi
+# mainline
 if [ -f /usr/bin/mainline ];then
         #rm -rf "$HOME"/.cache/mainline
         sudo rm -rf /root/.cache/mainline
@@ -23,6 +33,7 @@ if [ -f /usr/bin/mainline ];then
         else
         echo 'mainline não encontrado'
 fi
+# pkcon
 if [ -f /usr/bin/pkcon ];then
         if [ -f /usr/bin/aptitude ];then
                 if [ "$(aptitude search '~U')" ];then
