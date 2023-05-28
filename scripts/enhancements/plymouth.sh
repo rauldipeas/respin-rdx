cd80446c9b939f58e1e792606105a6441895bedd
Raul Dipeas <rdipeas+github@proton.me>
2023-05-28 04:55:04 -0300

    ^scripts

--- scripts/enhancements/plymouth.sh
+++ scripts/enhancements/plymouth.sh
@@ -9,10 +9,31 @@ chmod +x install
 cd ..
 rm -rf vortex-ubuntu-plymouth-theme
 if [ -f /KDeck ];then
-    rsvg-convert -w 376 -h 376 -o\
-    /usr/share/plymouth/themes/vortex-ubuntu/logo.png\
-    /usr/share/icons/Papirus/22x22/panel/steam_tray_mono.svg
-    else
-    wget -q --show-progress https://www.linuxadictos.com/en/wp-content/Uploads/ubuntu.svg
-    rsvg-convert -o /usr/share/plymouth/themes/vortex-ubuntu/logo.png ubuntu.svg
+    convert -density 500 -background none\
+    /usr/share/icons/Papirus/22x22/panel/steam_tray_mono.svg\
+    /usr/share/plymouth/themes/vortex-ubuntu/logo.png
+    elif [ -f /Kubuntu ];then
+    convert -density 500 -background none\
+    /usr/share/icons/Papirus/48x48/apps/distributor-logo-kubuntu.svg\
+    /usr/share/plymouth/themes/vortex-ubuntu/logo.png
+    elif [ -f /Xubuntu ];then
+    convert -density 500 -background none\
+    /usr/share/icons/Papirus/48x48/apps/distributor-logo-xubuntu.svg\
+    /usr/share/plymouth/themes/vortex-ubuntu/logo.png
+    elif [ -f /Neon ];then
+    convert -density 500 -background none\
+    /usr/share/icons/Papirus/48x48/apps/distributor-logo-neon.svg\
+    /usr/share/plymouth/themes/vortex-ubuntu/logo.png
+    elif [ -f /UCL ];then
+    convert -density 500 -background none\
+    /usr/share/icons/Papirus/48x48/apps/ubuntu-tweaks.svg\
+    /usr/share/plymouth/themes/vortex-ubuntu/logo.png
+    elif [ -f /UGL ];then
+    convert -density 500 -background none\
+    /usr/share/icons/Papirus/48x48/apps/desktop-environment-gnome.svg\
+    /usr/share/plymouth/themes/vortex-ubuntu/logo.png
+    elif [ -f /UGLR ];then
+    convert -density 500 -background none\
+    /usr/share/icons/Papirus/48x48/apps/desktop-environment-gnome.svg\
+    /usr/share/plymouth/themes/vortex-ubuntu/logo.png
 fi
