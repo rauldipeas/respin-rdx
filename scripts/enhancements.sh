29f30d25e1db2c0a3773d21fb5a15a91ff3ac74c
Raul Dipeas <rdipeas+github@proton.me>
2023-05-28 02:52:43 -0300

    ^scripts

--- scripts/enhancements.sh
+++ scripts/enhancements.sh
@@ -9,6 +9,6 @@ bash -x enhancements/rtcqs.sh
 bash -x enhancements/firefox.sh
 bash -x enhancements/betterbird.sh
 bash -x enhancements/telegram.sh
-bash -x enhancements/plymouth.sh
 bash -x enhancements/extras.sh
 bash -x enhancements/desktop.sh
+bash -x enhancements/plymouth.sh
