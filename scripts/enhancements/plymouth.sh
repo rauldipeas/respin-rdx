55bc9b13ad90abde2148469f1b4fcca7cdd74460
Raul Dipeas <rdipeas+github@proton.me>
2023-05-29 18:31:45 -0300

    ^scripts

--- scripts/enhancements/plymouth.sh
+++ scripts/enhancements/plymouth.sh
@@ -8,23 +8,24 @@ chmod +x install
 ./install
 cd ..
 rm -rf vortex-ubuntu-plymouth-theme
+sed -i 's/logo_image = logo_image.Scale (120 * s , 120 * s);/logo_image = logo_image.Scale (128 * s , 128 * s);/g' /usr/share/plymouth/themes/vortex-ubuntu/vortex-ubuntu.script
 if [ -f /KDeck ];then
     wget -q --show-progress https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/plymouth/kdeck.png
-    convert -background none kdeck.png /usr/share/plymouth/themes/vortex-ubuntu/logo.png
+    convert -density 1200 -resize 128x128 -background none kdeck.png /usr/share/plymouth/themes/vortex-ubuntu/logo.png
     elif [ -f /Kubuntu ];then
     wget -q --show-progress https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/plymouth/kubuntu.svg
-    convert -density 80 -background none kubuntu.svg /usr/share/plymouth/themes/vortex-ubuntu/logo.png
+    convert -density 1200 -resize 128x128 -background none kubuntu.svg /usr/share/plymouth/themes/vortex-ubuntu/logo.png
     elif [ -f /Neon ];then
     wget -q --show-progress https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/plymouth/neon.svg
-    convert -density 40 -background none neon.svg /usr/share/plymouth/themes/vortex-ubuntu/logo.png
+    convert -density 1200 -resize 128x128 -background none neon.svg /usr/share/plymouth/themes/vortex-ubuntu/logo.png
     elif [ -f /UCL ];then
     wget -q --show-progress https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/plymouth/ucl.svg
-    convert -density 400 -background none ucl.svg /usr/share/plymouth/themes/vortex-ubuntu/logo.png
+    convert -density 1200 -resize 128x128 -background none ucl.svg /usr/share/plymouth/themes/vortex-ubuntu/logo.png
     elif [ -f /Xubuntu ];then
     wget -q --show-progress https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/plymouth/xubuntu.svg
-    convert -density 20 -background none xubuntu.png /usr/share/plymouth/themes/vortex-ubuntu/logo.png
+    convert -density 1200 -resize 128x128 -background none xubuntu.svg /usr/share/plymouth/themes/vortex-ubuntu/logo.png
     else
-    wget -q --show-progress https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/plymouth/ubuntu.png
-    convert -density 20 -background none ubuntu.png /usr/share/plymouth/themes/vortex-ubuntu/logo.png
+    wget -q --show-progress https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/plymouth/ubuntu.svg
+    convert -density 1200 -resize 128x128 -background none ubuntu.svg /usr/share/plymouth/themes/vortex-ubuntu/logo.png
 fi
 update-initramfs -u -k all
