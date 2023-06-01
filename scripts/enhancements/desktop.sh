8608141efb254a632e50a3e4c20505eafd7bc8e3
Raul Dipeas <rdipeas+github@proton.me>
2023-06-01 20:25:05 -0300

    ^assets ^scripts

--- scripts/enhancements/desktop.sh
+++ scripts/enhancements/desktop.sh
@@ -441,15 +441,15 @@ export QT_QPA_PLATFORMTHEME=gtk2
 EOF
     rm -r /etc/skel/.config/libreoffice
     mkdir -p /etc/skel/.config/{autostart,gtk-3.0,smplayer,volumeicon,xfce4/panel,xfce4/xfconf/xfce-perchannel-xml}
-    wget -q --show-progress -O /etc/skel/.config/autostart/natural-scrolling.desktop https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/XFCE/natural-scrolling.desktop
-    wget -q --show-progress -O /etc/skel/.config/autostart/volumeicon.desktop https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/XFCE/volumeicon.desktop
-    wget -q --show-progress -O /etc/skel/.config/autostart/xcape.desktop https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/XFCE/xcape.desktop
-    sed -i 's/Hidden=true//g' /etc/skel/.config/autostart/volumeicon.desktop
+    wget -q --show-progress -O /etc/xdg/autostart/natural-scrolling.desktop https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/XFCE/natural-scrolling.desktop
+    wget -q --show-progress -O /etc/xdg/autostart/volumeicon.desktop https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/XFCE/volumeicon.desktop
+    sed -i 's/Hidden=true//g' /etc/xdg/autostart/volumeicon.desktop
+    wget -q --show-progress -O /etc/xdg/autostart/xcape.desktop https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/XFCE/xcape.desktop
     wget -q --show-progress -O /etc/skel/.config/gtk-3.0/gtk.css https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/XFCE/gtk.css
     wget -q --show-progress -O /etc/skel/.config/volumeicon/volumeicon https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/XFCE/volumeicon
     wget -q --show-progress -O /etc/skel/.config/xfce4/panel/docklike-2.rc https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/XFCE/docklike-2.rc
     wget -q --show-progress -O /etc/skel/.config/xfce4/panel/docklike-8.rc https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/XFCE/docklike-8.rc
-    wget -q --show-progress -O /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-appfinder.xml https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/XFCE/xfce4-appfinder.xml
+    wget -q --show-progress -O /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-appfinder.xml https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/XFCE/xfce4-appfinder.xml
     wget -q --show-progress -O /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/XFCE/xfce4-panel.xml
     wget -q --show-progress -O /usr/share/xfce4-panel-profiles/layouts/rauldipeas-rhino.tar.bz2 https://github.com/rauldipeas/respin-rdx/raw/main/assets/XFCE/rauldipeas-rhino.tar.bz2
     wget -q --show-progress -O /usr/share/xfce4-panel-profiles/layouts/rauldipeas-superbar.tar.bz2 https://github.com/rauldipeas/respin-rdx/raw/main/assets/XFCE/rauldipeas-superbar.tar.bz2
@@ -461,7 +461,7 @@ EOF
     sed -i '/tooltip-size/{n;s/  /\    <property name="gravity" type="int" value="2"\/\>\n \ /;}' /etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
     sed -i 's/name="show" type="bool" value="false/name="show" type="bool" value="true/g' /etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
     sed -i 's/xubuntu-wallpaper.png/xubuntu-impish.png/g' /etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
-    sed -i 's/&lt;Alt&gt;F3/&lt;Super&gt;x/g' /etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+    sed -i 's/\&lt;Alt\&gt;F3/\&lt;Super\&gt;x/g' /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
     sed -i 's/Alt/Super/g' /etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
     sed -i 's/Greybird/Greybird-dark/g' /etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
     sed -i 's/Noto Sans Bold/FreeSans Semi-Bold/g' /etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
