3ab91131aa4a129fcb4e99c50a559e1a7bf9abb7
Raul Dipeas <rdipeas+github@proton.me>
2023-06-16 17:47:33 -0300

    ^respin-rdx ^scripts

--- scripts/setup-host.sh
+++ scripts/setup-host.sh
@@ -3,3 +3,7 @@ set -e
 
 # Setup host
 sudo apt install -y mtools squashfs-tools
+
+# MEGA
+wget -q --show-progress https://mega.nz/linux/repo/xUbuntu_22.04/amd64/megacmd-xUbuntu_22.04_amd64.deb
+sudo apt install ./megacmd*.deb
