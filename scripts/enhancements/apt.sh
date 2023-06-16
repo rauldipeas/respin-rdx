5b745c6a8b7e4d57cbd7b14efedc792bddbc9c6d
Raul Dipeas <rdipeas+github@proton.me>
2023-06-16 16:27:58 -0300

    ^respin-rdx ^scripts

--- scripts/enhancements/apt.sh
+++ scripts/enhancements/apt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 # apt
-cat <<EOF |tee /etc/apt/apt.conf.d/force-confold
+cat <<EOF |tee /etc/apt/apt.conf.d/force-confold>/dev/null
 Dpkg::Options {
    "--force-confdef";
    "--force-confold";
