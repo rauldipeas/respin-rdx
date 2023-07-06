03c118ea3be21c364993e90ed3da40d76dc4de19
Raul Dipeas <rdipeas+github@proton.me>
2023-07-06 02:46:50 -0300

    ^scripts

--- scripts/enhancements/custom-spin/kavis.sh
+++ scripts/enhancements/custom-spin/kavis.sh
@@ -88,7 +88,7 @@ ColorSchemePath=
 EOF
 mkdir -p /etc/skel/.config/REAPER/{ColorThemes,LangPack,UserPlugins}
 wget -q --show-progress -O /etc/skel/.config/REAPER/ColorThemes/Default_6.0.ReaperThemeZip https://stash.reaper.fm/40797/Mammoth.ReaperThemeZip
-wget -q --show-progress -O /etc/skel/.config/REAPER/LangPack/pt-BR.ReaperLangPack https://stash.reaper.fm"$(wget -qO- https://stash.reaper.fm/tag/Language-Packs|grep pt-BR|head -n1|cut -d '"' -f2|sed 's/\/v//g')"
+wget -q --show-progress -O /etc/skel/.config/REAPER/LangPack/pt-BR.ReaperLangPack https://stash.reaper.fm"$(wget -qO- https://stash.reaper.fm/tag/Language-Packs|grep '/pt-BR.ReaperLangPack'|tail -n1|cut -d '"' -f2|sed 's/\/v//g')"
 wget -q --show-progress https://sws-extension.org/download/pre-release/"$(wget -qO- http://sws-extension.org/download/pre-release/|grep Linux-x86_64|head -n1|cut -d '"' -f4)"
 tar fx sws-*-Linux-x86_64-*.tar.xz -C /etc/skel/.config/REAPER
 wget -q --show-progress -O /etc/skel/.config/REAPER/UserPlugins/reaper_reapack-x86_64.so "$(wget -qO- https://api.github.com/repos/cfillion/reapack/releases|grep browser_download_url|grep download/v|grep x86_64.so|head -n1|cut -d '"' -f4)"
