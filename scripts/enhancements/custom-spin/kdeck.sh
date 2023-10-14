6ade427873a721171880e48bc0679a0b81feb78d
Raul Dipeas <rdipeas+github@proton.me>
2023-10-14 21:48:48 +0000

    ^kdeck

--- scripts/enhancements/custom-spin/kdeck.sh
+++ scripts/enhancements/custom-spin/kdeck.sh
@@ -20,31 +20,32 @@ apt autoremove --purge -y\
     skanlite*\
     usb-creator*\
     vlc*
-dpkg --add-architecture i386
+#dpkg --add-architecture i386
 #add-apt-repository -y ppa:libretro/stable
 apt update
-wget -q --show-progress https://cdn.akamai.steamstatic.com/client/installer/steam.deb
+#wget -q --show-progress https://cdn.akamai.steamstatic.com/client/installer/steam.deb
 apt install -y\
-    boilr\
     emulationstation-de\
-    filelight\
     gamemode\
-    libgl1-mesa-glx:i386\
-    mplayer\
-    smplayer\
-    ./steam.deb\
-    steamos-compositor-plus
+#    libgl1-mesa-glx:i386\
+#    boilr\
+#    filelight\
+#    mplayer\
 #    retroarch\
-apt update
-apt install -y\
-    steam-libs-amd64\
-    steam-libs-i386:i386
-flatpak install -y org.libretro.RetroArch
-wget -q --show-progress "$(wget -qO- --header="X-Auth-Token: $GITHUB_TOKEN" https://api.github.com/repos/flightlessmango/MangoHud/releases|grep browser_download_url|grep tar.gz|head -n1|cut -d '"' -f4)"
-tar fxz MangoHud*.tar.gz
-cd MangoHud
-./mangohud-setup.sh install
-
-#io.github.philipk.boilr
-#com.valvesoftware.Steam
-#org.freedesktop.Platform.VulkanLayer.MangoHud
+#    smplayer\
+#    ./steam.deb\
+#    steamos-compositor-plus
+#apt install -y\
+#    steam-libs-amd64\
+#    steam-libs-i386:i386
+#wget -q --show-progress "$(wget -qO- --header="X-Auth-Token: $GITHUB_TOKEN" https://api.github.com/repos/flightlessmango/MangoHud/releases|grep browser_download_url|grep tar.gz|head -n1|cut -d '"' -f4)"
+#tar fxz MangoHud*.tar.gz
+#cd MangoHud
+#./mangohud-setup.sh install
+flatpak install -y\
+    com.valvesoftware.Steam\
+    info.smplayer.SMPlayer\
+    io.github.philipk.boilr\
+    org.freedesktop.Platform.VulkanLayer.MangoHud\
+    org.kde.filelight\
+    org.libretro.RetroArch
--- scripts/enhancements/desktop/kdeck.sh
+++ scripts/enhancements/desktop/kdeck.sh
@@ -24,23 +24,23 @@ EOF
     wget -q --show-progress https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/KDE/breeze-alphablack-v20.zip
     unzip breeze-alphablack-v20.zip -d /usr/share/plasma/desktoptheme/breeze-alphablack
     echo 'gtk-application-prefer-dark-theme=true'|tee -a /etc/gtk-3.0/settings.ini
-    mkdir -p /etc/skel/.config/smplayer
-    wget -q --show-progress -O /etc/skel/.config/smplayer/smplayer.ini https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/smplayer/smplayer.ini
+    mkdir -p /etc/skel/.var/app/info.smplayer.SMPlayer/config/smplayer
+    wget -q --show-progress -O /etc/skel/.var/app/info.smplayer.SMPlayer/config/smplayer/smplayer.ini https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/smplayer/smplayer.ini
     sed -i 's/Yaru/Breeze/g' /etc/gtk-3.0/settings.ini
     git clone -q https://github.com/dragoonDorise/es-theme-epicnoir /usr/share/emulationstation/themes/epicnoir
-    mkdir -p /usr/local/share/applications
-    cp /usr/share/applications/steam.desktop /usr/local/share/applications/
-    sed -i 's@steam://open/bigpicture@-gamepadui@g' /usr/local/share/applications/steam.desktop
-    sed -i 's/Name=Big Picture/Name=Gamepad UI/g' /usr/local/share/applications/steam.desktop
-    sed -i 's/Categories=Network;/Categories=/g' /usr/local/share/applications/steam.desktop
-    cp /usr/share/applications/steam.desktop /etc/xdg/autostart/
-    sed -i 's/%U/-gamepadui/g' /etc/xdg/autostart/steam.desktop
+#    mkdir -p /usr/local/share/applications
+#    cp /usr/share/applications/steam.desktop /usr/local/share/applications/
+#    sed -i 's@steam://open/bigpicture@-gamepadui@g' /usr/local/share/applications/steam.desktop
+#    sed -i 's/Name=Big Picture/Name=Gamepad UI/g' /usr/local/share/applications/steam.desktop
+#    sed -i 's/Categories=Network;/Categories=/g' /usr/local/share/applications/steam.desktop
+#    cp /usr/share/applications/steam.desktop /etc/xdg/autostart/
+#    sed -i 's/%U/-gamepadui/g' /etc/xdg/autostart/steam.desktop
 #    sed -i 's/plasma/steamos/g' /var/lib/sddm/state.conf
-    mkdir -p /etc/sddm.conf.d
-    cat <<EOF |tee /etc/sddm.conf.d/kdeck.conf
-[Autologin]
-Session=/usr/share/xsessions/steamos.desktop
-EOF
+#    mkdir -p /etc/sddm.conf.d
+#    cat <<EOF |tee /etc/sddm.conf.d/kdeck.conf
+#[Autologin]
+#Session=/usr/share/xsessions/steamos.desktop
+#EOF
     git clone -q https://github.com/PapirusDevelopmentTeam/materia-kde
     cp -r materia-kde/plasma/desktoptheme/Materia/icons /usr/share/plasma/desktoptheme/breeze-alphablack/
     cp -r materia-kde/plasma/desktoptheme/Materia/icons /usr/share/plasma/desktoptheme/breeze-dark/
