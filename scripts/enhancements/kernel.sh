66050021fdb62de387fd20b7a01eb79b0c5809a2
Raul Dipeas <rdipeas+github@proton.me>
2023-12-26 18:59:59 +0000

    -xanmod +mainline

--- scripts/enhancements/kernel.sh
+++ scripts/enhancements/kernel.sh
@@ -9,35 +9,35 @@ apt autoremove --purge -y linux*generic*
 #update-initramfs -u -k "$(find /boot/|grep vmlinuz|grep -v old|tail -n1|sed 's@/boot/vmlinuz-@@g')"
 
 # XanMod
-wget -qO- https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/xanmod/archive.key|gpg --dearmor -o /etc/apt/trusted.gpg.d/xanmod-archive-keyring.gpg
-echo 'deb http://deb.xanmod.org releases main'|tee /etc/apt/sources.list.d/xanmod-release.list>/dev/null
-apt update
-apt install -y linux-xanmod-x64v3
+#wget -qO- https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/xanmod/archive.key|gpg --dearmor -o /etc/apt/trusted.gpg.d/xanmod-archive-keyring.gpg
+#echo 'deb http://deb.xanmod.org releases main'|tee /etc/apt/sources.list.d/xanmod-release.list>/dev/null
+#apt update
+#apt install -y linux-xanmod-x64v3
 
 # CFS Zen tweaks
 apt install -y cfs-zen-tweaks
 systemctl enable set-cfs-tweaks.service
 
 # Mainline
-#add-apt-repository -y ppa:cappelikan/ppa
-#cat <<EOF |tee /etc/apt/apt.conf.d/100mainline-icon
-#DPkg::Post-Invoke {"sed -i 's/Icon=mainline/Icon=mintsources-ppa/g' /usr/share/applications/mainline.desktop";};
-#EOF
-#apt install -y amd64-microcode intel-microcode mainline thermald
+add-apt-repository -y ppa:cappelikan/ppa
+cat <<EOF |tee /etc/apt/apt.conf.d/100mainline-icon
+DPkg::Post-Invoke {"sed -i 's/Icon=mainline/Icon=mintsources-ppa/g' /usr/share/applications/mainline.desktop";};
+EOF
+apt install -y amd64-microcode intel-microcode mainline thermald
 
 # Latest kernel
 #KERNEL_VER="v6.1.4/"
-#KERNEL_VER="$(wget -qO- https://kernel.ubuntu.com/~kernel-ppa/mainline/|grep folder|grep -v "\-rc"|tail -n1|cut -d '"' -f8)"
+KERNEL_VER="$(wget -qO- https://kernel.ubuntu.com/~kernel-ppa/mainline/|grep folder|grep -v "\-rc"|tail -n1|cut -d '"' -f8)"
 #KERNEL_VER="$(wget -qO- https://kernel.ubuntu.com/~kernel-ppa/mainline/|grep folder|grep -v "\-rc"|tail -n2|head -n1|cut -d '"' -f8)"
-#KERNEL_HEADERS_COMMON_FILE=$(wget -qO- https://kernel.ubuntu.com/~kernel-ppa/mainline/"$KERNEL_VER"|grep headers|grep all.deb|cut -d '"' -f2)
-#KERNEL_HEADERS_FILE=$(wget -qO- https://kernel.ubuntu.com/~kernel-ppa/mainline/"$KERNEL_VER"|grep headers|grep amd64.deb|cut -d '"' -f2)
-#KERNEL_IMAGE_FILE=$(wget -qO- https://kernel.ubuntu.com/~kernel-ppa/mainline/"$KERNEL_VER"|grep image|grep amd64.deb|cut -d '"' -f2)
-#KERNEL_MODULES_FILE=$(wget -qO- https://kernel.ubuntu.com/~kernel-ppa/mainline/"$KERNEL_VER"|grep modules|grep amd64.deb|cut -d '"' -f2)
-#wget -q --show-progress https://kernel.ubuntu.com/~kernel-ppa/mainline/"$KERNEL_VER""$KERNEL_HEADERS_COMMON_FILE"
-#wget -q --show-progress https://kernel.ubuntu.com/~kernel-ppa/mainline/"$KERNEL_VER""$KERNEL_HEADERS_FILE"
-#wget -q --show-progress https://kernel.ubuntu.com/~kernel-ppa/mainline/"$KERNEL_VER""$KERNEL_IMAGE_FILE"
-#wget -q --show-progress https://kernel.ubuntu.com/~kernel-ppa/mainline/"$KERNEL_VER""$KERNEL_MODULES_FILE"
-#apt install ./linux-headers*.deb ./linux-image*.deb ./linux-modules*.deb
+KERNEL_HEADERS_COMMON_FILE=$(wget -qO- https://kernel.ubuntu.com/~kernel-ppa/mainline/"$KERNEL_VER"|grep headers|grep all.deb|cut -d '"' -f2)
+KERNEL_HEADERS_FILE=$(wget -qO- https://kernel.ubuntu.com/~kernel-ppa/mainline/"$KERNEL_VER"|grep headers|grep amd64.deb|cut -d '"' -f2)
+KERNEL_IMAGE_FILE=$(wget -qO- https://kernel.ubuntu.com/~kernel-ppa/mainline/"$KERNEL_VER"|grep image|grep amd64.deb|cut -d '"' -f2)
+KERNEL_MODULES_FILE=$(wget -qO- https://kernel.ubuntu.com/~kernel-ppa/mainline/"$KERNEL_VER"|grep modules|grep amd64.deb|cut -d '"' -f2)
+wget -q --show-progress https://kernel.ubuntu.com/~kernel-ppa/mainline/"$KERNEL_VER""$KERNEL_HEADERS_COMMON_FILE"
+wget -q --show-progress https://kernel.ubuntu.com/~kernel-ppa/mainline/"$KERNEL_VER""$KERNEL_HEADERS_FILE"
+wget -q --show-progress https://kernel.ubuntu.com/~kernel-ppa/mainline/"$KERNEL_VER""$KERNEL_IMAGE_FILE"
+wget -q --show-progress https://kernel.ubuntu.com/~kernel-ppa/mainline/"$KERNEL_VER""$KERNEL_MODULES_FILE"
+apt install ./linux-headers*.deb ./linux-image*.deb ./linux-modules*.deb
 
 # ZSwap
 echo lz4|tee -a /etc/initramfs-tools/modules>/dev/null
