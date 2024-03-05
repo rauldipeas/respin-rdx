8570f97b6b82c1f73ebdd6c1152a7ff31208c924
Raul Dipeas <github.rauldipeas@lock.email>
2024-03-05 02:57:47 -0300

    -neon

--- scripts/actions-cleanup.sh
+++ scripts/actions-cleanup.sh
@@ -3,7 +3,7 @@ set -e
 
 # Actions cleanup
 PROJECT='rauldipeas/respin-rdx'
-KEEP=10
+KEEP=9
 gh api\
 	repos/"$PROJECT"/actions/runs\
 	--paginate -q '.workflow_runs[]|select(.head_branch != "any")|"\(.id)"'|tail -n+$((KEEP+1))\
--- scripts/enhancements/custom-spin/kdeck.sh
+++ scripts/enhancements/custom-spin/kdeck.sh
@@ -21,15 +21,24 @@ apt autoremove --purge -y\
     usb-creator*\
     vlc*
 dpkg --add-architecture i386
+#add-apt-repository -y ppa:libretro/stable
 apt update
+#wget -q --show-progress https://cdn.akamai.steamstatic.com/client/installer/steam.deb
 apt install -y\
     gamemode\
     steamos-compositor-plus
+#    ./steam.deb\
+#    boilr\
+#    emulationstation-de\
+#    filelight\
+#    mplayer\
+#    retroarch\
+#    smplayer\
 apt update
 apt install -y\
-    libgl1-mesa-glx:i386\
-    steam-libs-amd64\
-    steam-libs-i386:i386
+    libgl1-mesa-glx:i386
+    #steam-libs-amd64\
+    #steam-libs-i386:i386
 wget -q --show-progress "$(wget -qO- --header="X-Auth-Token: $GITHUB_TOKEN" https://api.github.com/repos/flightlessmango/MangoHud/releases|grep browser_download_url|grep tar.gz|head -n1|cut -d '"' -f4)"
 tar fxz MangoHud*.tar.gz
 cd MangoHud
@@ -39,3 +48,6 @@ flatpak install --assumeyes --noninteractive\
     info.smplayer.SMPlayer\
     io.github.philipk.boilr\
     org.kde.filelight
+#    com.steamgriddb.steam-rom-manager\
+#    org.libretro.RetroArch
+#    org.freedesktop.Platform.VulkanLayer.MangoHud//23.08\
