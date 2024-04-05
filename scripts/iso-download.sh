634a60eb9513218d0f6131f06c16330e9a245a84
Raul Dipeas <rauldipeas@disroot.org>
2024-04-05 16:03:20 -0300

    ^daily

--- scripts/iso-download.sh
+++ scripts/iso-download.sh
@@ -2,8 +2,16 @@
 
 # ISO download
 set -e
-if [ "$FLAVOUR" = ubuntu ]; then
-   wget -q https://releases.ubuntu.com/"$VERSION"/ubuntu-"$VERSION"-desktop-amd64.iso
+if [ "$VERSION" = daily ];then
+   if [ "$FLAVOUR" = ubuntu ]; then
+      wget -q https://cdimage.ubuntu.com/daily-live/current/"$CODENAME"-desktop-amd64.iso
+      else
+      wget -q https://cdimage.ubuntu.com/"$FLAVOUR"/daily-live/current/"$CODENAME"-desktop-amd64.iso
+   fi
    else
-   wget -q https://cdimage.ubuntu.com/"$FLAVOUR"/releases/"$CODENAME"/release/"$FLAVOUR"-"$VERSION"-desktop-amd64.iso
+   if [ "$FLAVOUR" = ubuntu ]; then
+      wget -q https://releases.ubuntu.com/"$VERSION"/ubuntu-"$VERSION"-desktop-amd64.iso
+      else
+      wget -q https://cdimage.ubuntu.com/"$FLAVOUR"/releases/"$CODENAME"/release/"$FLAVOUR"-"$VERSION"-desktop-amd64.iso
+   fi
 fi
