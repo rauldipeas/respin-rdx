8c7166d998572e26dc0bc8078486f829a9424216
Raul Dipeas <rauldipeas@disroot.org>
2024-04-06 15:56:30 -0300

    ^debootstrap

--- scripts/chroot.sh
+++ scripts/chroot.sh
@@ -13,41 +13,7 @@ sudo mount -o bind /proc "$CHROOT"/proc
 sudo mount -o bind /run "$CHROOT"/run
 sudo mount -o bind /sys "$CHROOT"/sys
 sudo touch "$CHROOT"/"$FLAVOUR_ID"
-## Desktop base
-echo "respin-rdx"|sudo tee "$CHROOT"/etc/hostname
-cat <<EOF |sudo tee "$CHROOT"/etc/apt/sources.list
-deb http://us.archive.ubuntu.com/ubuntu/ jammy main restricted universe multiverse
-deb-src http://us.archive.ubuntu.com/ubuntu/ jammy main restricted universe multiverse
-
-deb http://us.archive.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
-deb-src http://us.archive.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
-
-deb http://us.archive.ubuntu.com/ubuntu/ jammy-updates main restricted universe multiverse
-deb-src http://us.archive.ubuntu.com/ubuntu/ jammy-updates main restricted universe multiverse
-EOF
-cat <<EOF |sudo tee "$CHROOT"/etc/apt/preferences.d/nosnap.pref
-Package: snapd
-Pin: release a=*
-Pin-Priority: -10
-EOF
-sudo chroot "$CHROOT" apt update
-sudo chroot "$CHROOT" apt install -y sudo
-if [ -f /Kubuntu ]; then
-    echo 'Kubuntu'
-    sudo chroot "$CHROOT" apt install -y kubuntu-desktop
-    elif [ -f /KAVIS ]; then
-    echo 'Kubuntu Audio Video Image Studio'
-    sudo chroot "$CHROOT" apt install -y kubuntu-desktop
-    elif [ -f /KDeck ]; then
-    echo 'Kubuntu Deck'
-    sudo chroot "$CHROOT" apt install -y kubuntu-desktop
-    elif [ -f /Xubuntu ]; then
-    echo 'Xubuntu'
-    sudo chroot "$CHROOT" apt install -y xubuntu-desktop
-    else
-    echo 'Ubuntu'
-    sudo chroot "$CHROOT" apt install -y ubuntu-desktop
-fi
+bash -x scripts/base-desktop.sh
 ## Execução dos scripts de personalização
 sudo cp -r scripts/enhancements* "$CHROOT"/
 sudo chroot "$CHROOT" bash -x enhancements.sh
