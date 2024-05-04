ae25e6031c0682be0b5c18ce4d1d917606842ab1
Raul Dipeas <rauldipeas@disroot.org>
2024-05-03 21:41:40 -0300

    ^cadence

--- scripts/cadence.sh
+++ scripts/cadence.sh
@@ -21,9 +21,17 @@ if grep universe /etc/apt/sources.list.d/ubuntu.sources;then
     sudo add-apt-repository -ny universe
 fi
 sudo apt update 2>/dev/null
-if [ $(cut -d' ' -f4 <(grep "Nome do servidor" <(pactl info))) = pipewire ];then
+if [ $(cut -d' ' -f14 <(grep pipewire-pulse <(ps -e))) = pipewire-pulse ];then
     if  grep ii <(dpkg --list pulseaudio);then
         echo pulseaudio instalado!
+        systemctl --user mask pipewire.service pipewire.socket
+        systemctl --user stop pipewire.service pipewire.socket
+        systemctl --user disable pipewire.service pipewire.socket
+        sudo sed -i 's/NoDisplay=true/NoDisplay=false/g' /etc/xdg/autostart/pulseaudio.desktop
+        sudo sed -i 's/Systemd=true/Systemd=false/g' /etc/xdg/autostart/pulseaudio.desktop
+        sudo sed -i 's/start-pulseaudio-x11/systemctl --user start pulseaudio.service/g' /etc/xdg/autostart/pulseaudio.desktop
+        systemctl --user start pulseaudio.service pulseaudio.socket
+        grep 'Nome do servidor' <(pactl info)
         else
         sudo apt install -y pulseaudio
         systemctl --user mask pipewire.service pipewire.socket
@@ -33,10 +41,10 @@ if [ $(cut -d' ' -f4 <(grep "Nome do servidor" <(pactl info))) = pipewire ];then
         sudo sed -i 's/Systemd=true/Systemd=false/g' /etc/xdg/autostart/pulseaudio.desktop
         sudo sed -i 's/start-pulseaudio-x11/systemctl --user start pulseaudio.service/g' /etc/xdg/autostart/pulseaudio.desktop
         systemctl --user start pulseaudio.service pulseaudio.socket
-        grep "Nome do servidor" <(pactl info)
+        grep 'Nome do servidor' <(pactl info)
     fi
     else
-    grep "Nome do servidor" <(pactl info)
+    grep 'Nome do servidor' <(pactl info)
 fi
 if  grep ii <(dpkg --list alsa-firmware) && grep ii <(dpkg --list cadence) && grep ii <(dpkg --list pulseaudio) && grep ii <(dpkg --list pulseaudio-module-jack);then
     echo cadence instalado!
