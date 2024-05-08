a0051359f315e45d591c8de0a61eabb617c9fe11
Raul Dipeas <rauldipeas@disroot.org>
2024-05-07 22:15:23 -0300

    +hydra-launcher

---
+++ scripts/hydra-launcher.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+## CFS Zen tweaks
+if  grep ii <(dpkg --list hydra-launcher 2>/dev/null);then
+    echo hydra-launcher instalado!
+    else
+    rm -f hydra-launcher*.deb>/dev/null
+    wget -q --show-progress "$(wget -qO- https://api.github.com/repos/hydralauncher/hydra/releases|grep browser_download_url|grep .deb|head -n1|cut -d '"' -f4)"
+    sudo apt install -y ./hydra-launcher*.deb
+    rm hydra-launcher*.deb
+fi
+#wget -q --show-progress https://github.com/hydralauncher/hydra/releases/download/v1.1.0/hydra-launcher_1.1.0_amd64.deb
