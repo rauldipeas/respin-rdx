603a7d406fd1b78f34b38534eb4b4f1f9e4cca3c
Raul Dipeas <rauldipeas@disroot.org>
2024-05-12 01:42:06 -0300

    ^emudeck

--- scripts/emudeck.sh
+++ scripts/emudeck.sh
@@ -2,4 +2,13 @@
 set -e
 
 ## EmuDeck
-bash <(wget -qO- --show-progress https://www.emudeck.com/EmuDeck.sh)
+if  grep ii <(dpkg --list libfuse2t64 2>/dev/null);then
+    echo libfuse2t64 instalado!
+    else
+    sudo apt install -y	libfuse2t64
+fi
+mkdir -p "$HOME"/Applications
+wget -q --show-progress -O "$HOME"/Applications/EmuDeck.AppImage "$(wget -qO- https://api.github.com/repos/EmuDeck/emudeck-electron/releases|grep browser_download_url|grep .AppImage|head -n1|cut -d '"' -f4)"
+chmod +x "$HOME"/Applications/EmuDeck.AppImage
+"$HOME"/Applications/EmuDeck.AppImage --no-sandbox
+#bash <(wget -qO- --show-progress https://www.emudeck.com/EmuDeck.sh)
