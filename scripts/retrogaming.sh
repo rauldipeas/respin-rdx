42715089f061910c29d6baa28469d78c62cec524
Raul Dipeas <rauldipeas@disroot.org>
2024-05-14 00:52:04 -0300

    ^retrogaming

--- scripts/retrogaming.sh
+++ scripts/retrogaming.sh
@@ -13,11 +13,24 @@ if  grep ii <(dpkg --list libfuse2t64 2>/dev/null);then
     else
     sudo apt install -y	libfuse2t64
 fi
-if [ -f "$HOME"/.local/share/appimage/emulationstation.appimage ];then
+if [ -f "$HOME"/.EmulationStation/EmulationStation-DE-x64.AppImage ] && [ -f "$HOME"/.local/share/applications/emulationstation-de.desktop ];then
     echo emulationstation instalado!
     else
-    mkdir -p "$HOME"/.local/share/appimage
-    wget -q --show-progress -O "$HOME"/.local/share/appimage/emulationstation.appimage https://gitlab.com/es-de/emulationstation-de/-/package_files/127490092/download
-    chmod +x "$HOME"/.local/share/appimage/emulationstation.appimage
-    "$HOME"/.local/share/appimage/emulationstation.appimage
+    mkdir -p "$HOME"/.EmulationStation "$HOME"/.local/share/applications
+    wget -cq --show-progress -O "$HOME"/.EmulationStation/EmulationStation-DE-x64.AppImage https://gitlab.com/es-de/emulationstation-de/-/package_files/127490092/download
+    chmod +x "$HOME"/.EmulationStation/EmulationStation-DE-x64.AppImage
+cat <<EOF |tee "$HOME"/.local/share/applications/emulationstation-de.desktop>/dev/null
+[Desktop Entry]
+Version=1.0
+Exec=$HOME/.EmulationStation/EmulationStation-DE-x64.AppImage
+Icon=emulationstation
+Terminal=false
+Type=Application
+StartupNotify=true
+Hidden=false
+Categories=Game;Emulator;
+Name=EmulationStation Desktop Edition
+GenericName=Emulator Frontend
+Keywords=emulator;emulation;front-end;frontend;
+EOF
 fi
