5023f1776e064c6cd5ebb8a694f41bc5e18f7f50
Raul Dipeas <rauldipeas@disroot.org>
2024-05-14 05:36:31 -0300

    ^postinst

--- postinst.sh
+++ postinst.sh
@@ -4,14 +4,14 @@ set -e
 # postinst (media-center)
 ## Stremio
 bash <(wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/stremio.sh)
+## Retrogaming
+bash <(wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/retrogaming.sh)
 ## Steam
 bash <(wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/steam.sh)
 ## Heroic
 bash <(wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/heroic.sh)
 ## Hydra Launcher
 #bash <(wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/hydra-launcher.sh)
-## Retrogaming
-bash <(wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/retrogaming.sh)
 ## Papirus
 bash <(wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/papirus-icon-theme.sh)
 ## Dropbear
@@ -39,6 +39,12 @@ if  grep ii <(dpkg --list gnome-shell-extension-manager 2>/dev/null);then
     echo gnome-shell-extension-manager instalado!
     else
     sudo apt install -y gnome-shell-extension-manager
+fi
+## Kubuntu wallpapers
+if  grep ii <(dpkg --list kubuntu-wallpapers 2>/dev/null);then
+    echo kubuntu-wallpapers instalado!
+    else
+    sudo apt install -y kubuntu-wallpapers
 fi
 ## MESA utils
 if  grep ii <(dpkg --list mesa-utils 2>/dev/null);then
