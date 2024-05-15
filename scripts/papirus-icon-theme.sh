dbc6a6c0522ce1496afc0728add274443f3395ce
Raul Dipeas <rauldipeas@disroot.org>
2024-05-15 20:10:06 -0300

    ^papirus-icon-theme

--- scripts/papirus-icon-theme.sh
+++ scripts/papirus-icon-theme.sh
@@ -16,9 +16,11 @@ if  grep ii <(dpkg --list papirus-icon-theme 2>/dev/null);then
         else
         echo tuxguitar não encontrado!
     fi
-    if [ "$(echo $XDG_CURRENT_DESKTOP)" = ubuntu:GNOME ];then
+    if [ "$XDG_CURRENT_DESKTOP" = ubuntu:GNOME ];then
+        gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark
+        elif [ "$XDG_CURRENT_DESKTOP" = GNOME ];then
         gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark
         else
-        echo Ambiente gráfica não identificado.
+        echo Ambiente gráfico não identificado.
     fi
 fi
