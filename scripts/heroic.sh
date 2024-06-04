2e5e62cf0cb0d2df6de02003bcdf498ad1c51b56
Raul Dipeas <rauldipeas@disroot.org>
2024-06-04 04:12:06 -0300

    ^heroic

--- scripts/heroic.sh
+++ scripts/heroic.sh
@@ -2,11 +2,36 @@
 set -e
 
 ## Heroic
-if  grep ii <(dpkg --list heroic 2>/dev/null);then
+#if  grep ii <(dpkg --list heroic 2>/dev/null);then
+#    echo heroic instalado!
+#    else
+#    rm -rf heroic*.deb>/dev/null
+#    wget -q --show-progress "$(wget -qO- https://api.github.com/repos/Heroic-Games-Launcher/HeroicGamesLauncher/releases|grep browser_download_url|grep .deb|head -n1|cut -d '"' -f4)"
+#    sudo apt install -y ./heroic*.deb
+#    rm heroic*.deb
+#fi
+if  grep ii <(dpkg --list libfuse2t64 2>/dev/null);then
+    echo libfuse2t64 instalado!
+    else
+    sudo apt install -y	libfuse2t64
+fi
+if [ -f "$HOME"/Applications/heroic.AppImage ] && [ -f "$HOME"/.local/share/applications/heroic.desktop ];then
     echo heroic instalado!
     else
-    rm -rf heroic*.deb>/dev/null
-    wget -q --show-progress "$(wget -qO- https://api.github.com/repos/Heroic-Games-Launcher/HeroicGamesLauncher/releases|grep browser_download_url|grep .deb|head -n1|cut -d '"' -f4)"
-    sudo apt install -y ./heroic*.deb
-    rm heroic*.deb
+    mkdir -p "$HOME"/Applications "$HOME"/.local/share/applications
+    wget -cq --show-progress -O "$HOME"/Applications/heroic.AppImage "$(wget -qO- https://api.github.com/repos/Heroic-Games-Launcher/HeroicGamesLauncher/releases|grep browser_download_url|grep .AppImage|head -n1|cut -d '"' -f4)"
+    chmod +x "$HOME"/Applications/heroic.AppImage
+cat <<EOF |tee "$HOME"/.local/share/applications/heroic.desktop>/dev/null
+[Desktop Entry]
+Version=1.0
+Exec=$HOME/Applications/heroic.AppImage --no-sandbox
+Icon=heroic
+Terminal=false
+Type=Application
+StartupNotify=true
+Hidden=false
+Categories=Game;
+Name=Heroic Games Launcher
+Keywords=game;epic;
+EOF
 fi
