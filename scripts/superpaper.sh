169b69b7f139ccd1dda015a2026d839889899fe7
Raul Dipeas <rauldipeas@disroot.org>
2024-07-24 14:35:06 -0300

    ^superpaper

--- scripts/superpaper.sh
+++ scripts/superpaper.sh
@@ -20,9 +20,9 @@ if  grep superpaper <(pipx list --short 2>/dev/null);then
     pipx install --system-site-packages superpaper
     sed -i 's@/usr/bin/python3 -m superpaper@superpaper@g' "$HOME"/.local/pipx/venvs/superpaper/share/applications/superpaper.desktop
     mkdir -p "$HOME"/.local/share/{applications,icons/hicolor/256x256/apps}
-    ln -fs "$HOME"/.local/share/pipx/venvs/superpaper/share/icons/hicolor/256x256/apps/superpaper.png "$HOME"/.local/share/icons/hicolor/256x256/apps/superpaper.png
-    ln -fs "$HOME"/.local/share/pipx/venvs/superpaper/share/applications/superpaper.desktop "$HOME"/.local/share/applications/superpaper.desktop
-    ln -fs "$HOME"/.local/share/pipx/venvs/superpaper/share/applications/superpaper.desktop "$HOME"/.config/autostart/superpaper.desktop
+    ln -fs "$HOME"/.local/pipx/venvs/superpaper/share/icons/hicolor/256x256/apps/superpaper.png "$HOME"/.local/share/icons/hicolor/256x256/apps/superpaper.png
+    ln -fs "$HOME"/.local/pipx/venvs/superpaper/share/applications/superpaper.desktop "$HOME"/.local/share/applications/superpaper.desktop
+    ln -fs "$HOME"/.local/pipx/venvs/superpaper/share/applications/superpaper.desktop "$HOME"/.config/autostart/superpaper.desktop
 fi
 
 
