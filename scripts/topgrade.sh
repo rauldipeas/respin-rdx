30424d4d517340ed9e312437fcb920f56faec8e8
Raul Dipeas <rauldipeas@disroot.org>
2024-07-24 14:24:54 -0300

    ^topgrade

--- scripts/topgrade.sh
+++ scripts/topgrade.sh
@@ -6,7 +6,7 @@ if [ -f /usr/bin/topgrade ];then
     echo topgrade instalado!
     else
     rm -f topgrade*.tar.gz>/dev/null
-    wget -q --show-progress "$(wget -qO- https://api.github.com/repos/topgrade-rs/topgrade/releases|grep browser_download_url|grep linux-gnu.tar.gz|head -n1|cut -d '"' -f4)"
+    wget -q --show-progress "$(wget -qO- https://api.github.com/repos/topgrade-rs/topgrade/releases|grep browser_download_url|grep linux-gnu.tar.gz|grep x86_64|head -n1|cut -d '"' -f4)"
     sudo tar -xf topgrade*.tar.gz -C /usr/bin/
     rm topgrade*.tar.gz
 fi
