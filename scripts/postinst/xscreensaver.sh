5ff37bced965e6fd0da8eb25a9598934399ff1b6
Raul Dipeas <rauldipeas@disroot.org>
2024-08-10 00:16:13 -0300

    ^xscreensaver

--- scripts/postinst/xscreensaver.sh
+++ scripts/postinst/xscreensaver.sh
@@ -6,13 +6,6 @@ if  grep ii <(dpkg --list xscreensaver 2>/dev/null) && grep ii <(dpkg --list xsc
     echo xscreensaver instalado!
     else
     sudo apt install -y xscreensaver xscreensaver-data-extra xscreensaver-gl-extra
-    cat <<EOF |sudo tee /etc/xdg/autostart/xscreensaver.desktop>/dev/null
-[Desktop Entry]
-Exec=xscreensaver --no-splash
-Icon=xscreensaver
-Name=XScreenSaver
-Type=Application
-Terminal=false
-X-Ubuntu-Gettext-Domain=xscreensaver
-EOF
+    systemctl --user enable xscreensaver.service
+    systemctl --user start xscreensaver.service
 fi
