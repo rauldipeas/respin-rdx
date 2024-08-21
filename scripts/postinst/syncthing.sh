a901a7203c8dcb2d00cc98610cf997deb69893dc
Raul Dipeas <rauldipeas@disroot.org>
2024-08-21 20:43:02 -0300

    ^syncthing ^gnome-tweaks

--- scripts/postinst/syncthing.sh
+++ scripts/postinst/syncthing.sh
@@ -2,8 +2,8 @@
 set -e
 
 ## Syncthing
-if  grep ii <(dpkg --list syncthing 2>/dev/null);then
+if  grep ii <(dpkg --list syncthing 2>/dev/null)&&grep ii <(dpkg --list syncthingtray 2>/dev/null);then
     echo syncthing instalado!
     else
-    sudo apt install -y syncthing
+    sudo apt install -y syncthing syncthingtray
 fi
