80ea0ca675e01545c4110bc0fbb09b92acf83d25
Raul Dipeas <rauldipeas@disroot.org>
2024-08-27 23:05:17 -0300

    ^syncthing

--- scripts/postinst/syncthing.sh
+++ scripts/postinst/syncthing.sh
@@ -5,5 +5,8 @@ set -e
 if  grep ii <(dpkg --list syncthing 2>/dev/null)&&grep ii <(dpkg --list syncthingtray 2>/dev/null);then
     echo syncthing instalado!
     else
+    wget -qO- https://syncthing.net/release-key.gpg|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/syncthing-archive-keyring.gpg>/dev/null
+    echo 'deb https://apt.syncthing.net/ syncthing stable'|sudo tee /etc/apt/sources.list.d/syncthing.list>/dev/null
+    sudo apt update
     sudo apt install -y syncthing syncthingtray
 fi
