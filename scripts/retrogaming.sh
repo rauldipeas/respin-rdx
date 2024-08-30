e9dc1d03cb5a8daa37655a70e46fd2fbdb586552
Raul Dipeas <rauldipeas@disroot.org>
2024-08-30 06:59:52 -0300

    ^retrogaming

--- scripts/retrogaming.sh
+++ scripts/retrogaming.sh
@@ -4,18 +4,18 @@ set -e
 ## Retrogaming
 ### RetroArch
 bash <(wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/retroarch.sh)
-### DuckStation
-bash <(wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/duckstation.sh)
 ### Cemu
 bash <(wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/cemu.sh)
 ### Dolphin Emulator
 bash <(wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/dolphin-emu.sh)
+### DuckStation
+bash <(wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/duckstation.sh)
 ### Flycast
 bash <(wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/flycast.sh)
 ### Kega Fusion
 bash <(wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/kega-fusion.sh)
 ### Mupen64Plus
-bash <(wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/mupen64plus.sh)
+bash <(wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/mupen64plus-qt.sh)
 ### PCSX2
 bash <(wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/pcsx2.sh)
 ### RPCS3
@@ -25,6 +25,6 @@ bash <(wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/vita3
 ### XEmu
 bash <(wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/xemu.sh)
 ### Yabause
-bash <(wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/yabauset.sh)
+bash <(wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/yabause.sh)
 ### EmulationStation DE
 bash <(wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/emulationstation-de.sh)
