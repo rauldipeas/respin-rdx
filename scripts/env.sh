14fd0146c8d8a17c766e6b5d5eb4d4a560a91a51
Raul Dipeas <github.rauldipeas@lock.email>
2024-11-04 11:30:40 -0300

    ^env ^respin-rdx

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-rm -f debian-archive-keyring*.deb live-build*.deb>/dev/null
+#rm -f debian-archive-keyring*.deb live-build*.deb>/dev/null
+sudo apt install -y wget
 wget -q --show-progress http://ftp.us.debian.org/debian/pool/main/d/debian-archive-keyring/debian-archive-keyring_2023.3+deb12u1_all.deb
 wget -q --show-progress http://ftp.us.debian.org/debian/pool/main/l/live-build/live-build_20230502_all.deb
 sudo apt install -y ./debian-archive-keyring*.deb ./live-build*.deb
