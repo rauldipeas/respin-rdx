7891674096107241d75afbbec27c1c5eab25ac2e
Raul Dipeas <github.rauldipeas@lock.email>
2024-11-04 10:22:30 -0300

    ^kernel ^lb-config

--- scripts/lb-config.sh
+++ scripts/lb-config.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 set -e
 cd /tmp/respin-rdx
-lb config \
-    --apt-source-archives false \
-    --architectures amd64 \
-    --archive-areas "main contrib non-free non-free-firmware" \
-    --backports true \
-    --bootappend-live "boot=live components cpufreq.default_governor=performance mitigations=off preempt=full quiet splash threadirqs vt.global_cursor_default=0 zswap.enabled=1 zswap.compressor=lz4 zswap.max_pool_percent=20 zswap.zpool=z3fold" \
-    --chroot-squashfs-compression-type xz \
-    --compression xz \
-    --debconf-frontend noninteractive \
-    --debian-installer none \
-    --distribution bookworm \
-    --image-name "respin-rdx" \
-    --iso-application "Respin RDX" \
-    --iso-publisher "Respin RDX; https://respin-rdx.sourceforge.io; respin-rdx.rauldipeas@lock.email" \
-    --iso-volume "Respin RDX" \
-    --linux-packages none\
-    --mirror-bootstrap "https://deb.debian.org/debian/" \
-    --mirror-binary "https://deb.debian.org/debian/" \
-    --mirror-chroot "https://deb.debian.org/debian/" \
-    --system live
+lb config\
+    --apt-source-archives false\
+    --architectures amd64\
+    --archive-areas "main contrib non-free non-free-firmware"\
+    --backports true\
+    --bootappend-live "boot=live components cpufreq.default_governor=performance mitigations=off preempt=full quiet splash threadirqs vt.global_cursor_default=0 zswap.enabled=1 zswap.compressor=lz4 zswap.max_pool_percent=20 zswap.zpool=z3fold"\
+    --checksums md5\
+    --chroot-squashfs-compression-type xz\
+    --compression xz\
+    --debconf-frontend noninteractive\
+    --debian-installer none\
+    --distribution bookworm\
+    --image-name "respin-rdx"\
+    --iso-application "Respin RDX"\
+    --iso-publisher "Respin RDX; https://respin-rdx.sourceforge.io; respin-rdx.rauldipeas@lock.email"\
+    --iso-volume "Respin RDX"\
+    --linux-packages "linux-image linux-headers -t bookworm-backports"\
+    --mirror-bootstrap "https://deb.debian.org/debian/"\
+    --mirror-binary "https://deb.debian.org/debian/"\
+    --mirror-chroot "https://deb.debian.org/debian/"\
+    --quiet\
+    --system live\
+    --utc-time true\
