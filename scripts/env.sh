64bb931b597567d21075e5acd5244002ffdd92da
Raul Dipeas <github.rauldipeas@lock.email>
2024-11-04 11:44:04 -0300

    ^env

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
-apt update
-apt install -y dpkg-dev wget
 wget -q --show-progress http://ftp.us.debian.org/debian/pool/main/d/debian-archive-keyring/debian-archive-keyring_2023.3+deb12u1_all.deb
 wget -q --show-progress http://ftp.us.debian.org/debian/pool/main/l/live-build/live-build_20230502_all.deb
 apt install -y ./debian-archive-keyring*.deb ./live-build*.deb
