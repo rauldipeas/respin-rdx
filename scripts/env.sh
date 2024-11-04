a38298a590c6f0766423362ad06ad73972e3640d
Raul Dipeas <github.rauldipeas@lock.email>
2024-11-04 12:00:15 -0300

    ^env ^respin-rdx

--- scripts/env.sh
+++ scripts/env.sh
@@ -2,5 +2,6 @@
 set -e
 wget -q --show-progress http://ftp.us.debian.org/debian/pool/main/d/debian-archive-keyring/debian-archive-keyring_2023.3+deb12u1_all.deb
 wget -q --show-progress http://ftp.us.debian.org/debian/pool/main/l/live-build/live-build_20230502_all.deb
+sudo apt update
 sudo apt install -y ./debian-archive-keyring*.deb ./live-build*.deb
 mkdir -p /tmp/respin-rdx
