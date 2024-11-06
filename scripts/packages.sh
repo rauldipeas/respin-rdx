bf0be341ba24b0568e797b60e829534e98f691b8
Raul Dipeas <github.rauldipeas@lock.email>
2024-11-06 11:27:28 -0300

    ^desktop-packages

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -17,6 +17,8 @@ dpkg-name gpu-viewer*.deb
 #micro
 wget -q --show-progress "$(wget -qO- https://api.github.com/repos/zyedidia/micro/releases|grep browser_download_url|grep amd64.deb|head -n1|cut -d '"' -f4)"
 dpkg-name micro*.deb
+#tribler
+wget -q --show-progress "$(wget -qO- https://api.github.com/repos/Tribler/tribler/releases|grep browser_download_url|grep amd64.deb|head -n1|cut -d '"' -f4)"
 #veracrypt
 wget -q --show-progress "$(wget -qO- https://www.veracrypt.fr/en/Downloads.html|grep amd64.deb|head -n1|cut -d '"' -f2|sed 's|&#43;|+|')"
 dpkg-name veracrypt*.deb
