9aa8410e52073d62a22f5c0ea29eb681b04e8625
Raul Dipeas <github.rauldipeas@lock.email>
2024-11-08 01:40:31 -0300

    -enpass

--- scripts/hooks/apt/enpass.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-wget -qO- https://apt.enpass.io/keys/enpass-linux.key|gpg --dearmor -o /etc/apt/trusted.gpg.d/enpass.gpg>/dev/null
-echo 'deb https://apt.enpass.io/  stable main'|tee /etc/apt/sources.list.d/enpass.list>/dev/null
-apt update
-apt install -y enpass
