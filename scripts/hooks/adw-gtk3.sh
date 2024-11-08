84233a3fd9e2e206e7415250ef69b1da7f919cbf
Raul Dipeas <github.rauldipeas@lock.email>
2024-11-08 13:22:09 -0300

    +adw-gtk3 ^gdm ^dconf

---
+++ scripts/hooks/adw-gtk3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+wget -q --show-progress "$(wget -qO- https://api.github.com/repos/lassekongo83/adw-gtk3/releases|grep browser_download_url|grep .tar.xz|head -n1|cut -d '"' -f4)"
+mkdir -p /usr/local/share/themes
+tar -xf adw-gtk3*.tar.xz -C /usr/local/share/themes/
+rm adw-gtk3*.tar.xz
+flatpak install org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark
+snap install adw-gtk3-theme
--- scripts/hooks/apt/gdm.sh
+++ scripts/hooks/apt/gdm.sh
@@ -4,10 +4,16 @@ apt install -y git imagemagick
 git clone --depth=1 --single-branch https://github.com/realmazharhussain/gdm-tools
 cd gdm-tools
 echo y|./install.sh
-sed -i 's|IconTheme=Adwaita|Papirus-Dark|' /usr/share/themes/Adwaita/index.theme
-sed -i 's|IconTheme=Adwaita|Papirus-Dark|' /usr/share/themes/Adwaita-dark/index.theme
-convert /usr/share/backgrounds/unsplash/unsplash-gnome.jpg -blur 0x80 /usr/share/backgrounds/unsplash/blured_unsplash-gnome.jpg
-set-gdm-theme set default /usr/share/backgrounds/unsplash/blured_unsplash-gnome.jpg
+#sed -i 's|IconTheme=Adwaita|IconTheme=Papirus|' /usr/share/themes/Adwaita/index.theme
+#sed -i 's|IconTheme=Adwaita|IconTheme=Papirus-Dark|' /usr/share/themes/Adwaita-dark/index.theme
+cat <<EOF |tee -a /usr/local/share/themes/adw-gtk3/index.theme
+IconTheme=Papirus
+EOF
+cat <<EOF |tee -a  /usr/local/share/themes/adw-gtk3-dark/index.theme
+IconTheme=Papirus-Dark
+EOF
+convert /usr/share/backgrounds/unsplash/unsplash-gnome.jpg -blur 0x80 /usr/share/backgrounds/gdm/blured_unsplash-gnome.jpg
+set-gdm-theme set adw-gtk3-dark /usr/share/backgrounds/gdm/blured_unsplash-gnome.jpg
 echo y|./uninstall.sh --purge
 cd ..
 rm -rf gdm-tools
