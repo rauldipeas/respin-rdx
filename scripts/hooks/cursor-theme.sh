476d1ce733fede769f7f82fbc0719a33c81f6290
Raul Dipeas <github.rauldipeas@lock.email>
2024-11-11 03:01:36 -0300

    ^cursor-theme

--- scripts/hooks/cursor-theme.sh
+++ scripts/hooks/cursor-theme.sh
@@ -3,4 +3,5 @@ set -e
 wget -q --show-progress https://github.com/rauldipeas/respin-rdx/raw/main/assets/compressed/Bibata.tar.xz
 tar -xf Bibata.tar.xz
 mv Bibata-* /usr/share/icons/
-update-alternatives --set x-cursor-theme /usr/share/icons/Bibata-Modern-Ice/index.theme
+update-alternatives --install /etc/alternative/x-cursor-theme x-cursor-theme /usr/share/icons/Bibata-Modern-Ice/cursor.theme 100
+update-alternatives --set x-cursor-theme /usr/share/icons/Bibata-Modern-Ice/cursor.theme
