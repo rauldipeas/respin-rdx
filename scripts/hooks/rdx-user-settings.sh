1d14569fa96a3d7288cd90dde7e7ec25b9379f5b
Raul Dipeas <github.rauldipeas@lock.email>
2024-11-15 00:49:05 -0300

    ^rdx-user-settings

--- scripts/hooks/rdx-user-settings.sh
+++ scripts/hooks/rdx-user-settings.sh
@@ -15,16 +15,16 @@ cp /opt/rdx-user-settings/adaptive.xml /usr/local/share/gtksourceview-4/styles/a
 wget -qO /opt/rdx-user-settings/topgrade-config.toml https://github.com/topgrade-rs/topgrade/raw/main/config.example.toml
 sed -i 's|# no_self_update|no_self_update|' /opt/rdx-user-settings/topgrade-config.toml
 sed -i 's|\[deno\]|#\[deno\]|' /opt/rdx-user-settings/topgrade-config.toml
-wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/hooks/rdx-user-settings/tribler.desktop|tee -a /usr/share/applications/org.tribler.Tribler.desktop>/dev/null
-wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/hooks/rdx-user-settings/yad.desktop|tee -a /usr/share/applications/yad-icon-browser.desktop>/dev/null
-wget -qO /usr/local/share/applications/respinrdx.featurebase.app.desktop https://github.com/rauldipeas/respin-rdx/raw/main/scripts/hooks/rdx-user-settings/featurebase.desktop
-wget -qO /usr/local/share/applications/browser-installer.desktop https://github.com/rauldipeas/respin-rdx/raw/main/scripts/hooks/rdx-user-settings/browser-installer.desktop
-wget -qO /usr/local/bin/browser-installer https://github.com/rauldipeas/respin-rdx/raw/main/scripts/hooks/rdx-user-settings/browser-installer.bash
+wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/hooks/rdx-user-settings.d/tribler.desktop|tee -a /usr/share/applications/org.tribler.Tribler.desktop>/dev/null
+wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/hooks/rdx-user-settings.d/yad.desktop|tee -a /usr/share/applications/yad-icon-browser.desktop>/dev/null
+wget -qO /usr/local/share/applications/respinrdx.featurebase.app.desktop https://github.com/rauldipeas/respin-rdx/raw/main/scripts/hooks/rdx-user-settings.d/featurebase.desktop
+wget -qO /usr/local/share/applications/browser-installer.desktop https://github.com/rauldipeas/respin-rdx/raw/main/scripts/hooks/rdx-user-settings.d/browser-installer.desktop
+wget -qO /usr/local/bin/browser-installer https://github.com/rauldipeas/respin-rdx/raw/main/scripts/hooks/rdx-user-settings.d/browser-installer.bash
 chmod +x /usr/local/bin/browser-installer
-wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/hooks/rdx-user-settings/rdx-user-settings.bash|tee /etc/profile.d/rdx-user-settings.sh /etc/X11/Xsession.d/90-rdx-user-settings>/dev/null
-wget -qO /etc/polkit-1/localauthority/50-local.d/50-udisks.pkla https://github.com/rauldipeas/respin-rdx/raw/main/scripts/hooks/rdx-user-settings/udisks.pkla
-wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/hooks/rdx-user-settings/qt-qpa-platformtheme.bash|tee /etc/environment.d/90-qt-qpa-platformtheme.conf /etc/profile.d/qt-qpa-platformtheme.sh /etc/X11/Xsession.d/90-qt-qpa-platformtheme>/dev/null
-wget -qO /etc/rc.local https://github.com/rauldipeas/respin-rdx/raw/main/scripts/hooks/rdx-user-settings/rc.bash
+wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/hooks/rdx-user-settings.d/rdx-user-settings.bash|tee /etc/profile.d/rdx-user-settings.sh /etc/X11/Xsession.d/90-rdx-user-settings>/dev/null
+wget -qO /etc/polkit-1/localauthority/50-local.d/50-udisks.pkla https://github.com/rauldipeas/respin-rdx/raw/main/scripts/hooks/rdx-user-settings.d/udisks.pkla
+wget -qO- https://github.com/rauldipeas/respin-rdx/raw/main/scripts/hooks/rdx-user-settings.d/qt-qpa-platformtheme.bash|tee /etc/environment.d/90-qt-qpa-platformtheme.conf /etc/profile.d/qt-qpa-platformtheme.sh /etc/X11/Xsession.d/90-qt-qpa-platformtheme>/dev/null
+wget -qO /etc/rc.local https://github.com/rauldipeas/respin-rdx/raw/main/scripts/hooks/rdx-user-settings.d/rc.bash
 chmod +x /etc/rc.local
-wget -qO /usr/local/bin/reset-user-settings https://github.com/rauldipeas/respin-rdx/raw/main/scripts/hooks/rdx-user-settings/reset-user-settings.bash
+wget -qO /usr/local/bin/reset-user-settings https://github.com/rauldipeas/respin-rdx/raw/main/scripts/hooks/rdx-user-settings.d/reset-user-settings.bash
 chmod +x /usr/local/bin/reset-user-settings
