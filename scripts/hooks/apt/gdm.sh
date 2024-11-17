b6b1850fbca10d02e8667e5ba3ca77f07bd1551b
Raul Dipeas <github.rauldipeas@lock.email>
2024-11-16 22:09:08 -0300

    +greeter -cursor-theme ^dconf ^desktop-packages ^gdm

--- scripts/hooks/apt/gdm.sh
+++ scripts/hooks/apt/gdm.sh
@@ -1,28 +1,7 @@
 #!/bin/bash
 set -e
-apt install -y git imagemagick
-git clone --depth=1 --single-branch https://github.com/realmazharhussain/gdm-tools
-cd gdm-tools
-echo y|./install.sh
-sed -i 's|IconTheme=Adwaita|IconTheme=Papirus|' /usr/share/themes/Adwaita/index.theme
-sed -i 's|IconTheme=Adwaita|IconTheme=Papirus-Dark|' /usr/share/themes/Adwaita-dark/index.theme
-sed -i 's|Tela-circle|Papirus|'\
-    /usr/share/themes/Orchis/index.theme\
-    /usr/share/themes/Orchis-Compact/index.theme\
-    /usr/share/themes/Orchis-Dark/index.theme\
-    /usr/share/themes/Orchis-Dark-Compact/index.theme\
-    /usr/share/themes/Orchis-Light/index.theme\
-    /usr/share/themes/Orchis-Light-Compact/index.theme\
-cat <<EOF |tee -a /usr/local/share/themes/adw-gtk3/index.theme
-IconTheme=Papirus
-EOF
-cat <<EOF |tee -a  /usr/local/share/themes/adw-gtk3-dark/index.theme
-IconTheme=Papirus-Dark
-EOF
+apt install -y imagemagick
 mkdir -p /usr/share/backgrounds/gdm
-convert /usr/share/backgrounds/unsplash/unsplash-gnome.jpg -blur 0x80 /usr/share/backgrounds/gdm/blured_unsplash-gnome.jpg
-set-gdm-theme set Orchis-Dark-Compact /usr/share/backgrounds/gdm/blured_unsplash-gnome.jpg
-echo y|./uninstall.sh --purge
-cd ..
-rm -rf gdm-tools
-apt autoremove --purge -y git imagemagick
+convert /usr/share/backgrounds/unsplash/john-towner-JgOeRuGD_Y4-unsplash.jpg -blur 0x80 /usr/share/backgrounds/gdm/blurred_unsplash-gdm.jpg
+apt autoremove --purge -y imagemagick
+wget -qO- /etc/gdm3/greeter.dconf-defaults https://github.com/rauldipeas/respin-rdx/raw/main/settings/dconf-greeter.ini
--- scripts/hooks/cursor-theme.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-wget -q --show-progress https://github.com/rauldipeas/respin-rdx/raw/main/assets/compressed/Bibata.tar.xz
-tar -xf Bibata.tar.xz
-mv Bibata-* /usr/share/icons/
-update-alternatives --install /usr/share/icons/default/index.theme x-cursor-theme /usr/share/icons/Bibata-Modern-Ice/cursor.theme 100
-update-alternatives --set x-cursor-theme /usr/share/icons/Bibata-Modern-Ice/cursor.theme
