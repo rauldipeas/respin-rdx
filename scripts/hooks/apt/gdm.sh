99e803f85a414d6c5f187bb6854766c69ab93742
Raul Dipeas <github.rauldipeas@lock.email>
2024-11-17 02:51:22 -0300

    ^dconf ^gdm

--- scripts/hooks/apt/gdm.sh
+++ scripts/hooks/apt/gdm.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -e
-apt install -y imagemagick
+apt install -y git imagemagick
+git clone --depth=1 --single-branch https://github.com/realmazharhussain/gdm-tools
+cd gdm-tools
+echo y|./install.sh
 mkdir -p /usr/share/backgrounds/gdm
-convert /usr/share/backgrounds/unsplash/john-towner-JgOeRuGD_Y4-unsplash.jpg -blur 0x60 /usr/share/backgrounds/gdm/blurred_unsplash-gdm.jpg
-apt autoremove --purge -y imagemagick
+convert /usr/share/backgrounds/unsplash/unsplash-gnome.jpg -blur 0x80 /usr/share/backgrounds/gdm/blurred_unsplash-gdm.jpg
+set-gdm-theme set Orchis-Dark-Compact /usr/share/backgrounds/gdm/blurred_unsplash-gdm.jpg
+echo y|./uninstall.sh --purge
+cd ..
+rm -rf gdm-tools
+apt autoremove --purge -y git imagemagick
 wget -qO /etc/gdm3/greeter.dconf-defaults https://github.com/rauldipeas/respin-rdx/raw/main/settings/dconf-greeter.ini
