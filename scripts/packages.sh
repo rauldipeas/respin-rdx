dd29d56ce634fe68b8b5530941ff8808aeadd140
Raul Dipeas <github.rauldipeas@lock.email>
2024-11-22 10:22:47 -0300

    ^gnome-shell-extensions ^packages

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -17,6 +17,9 @@ dpkg-name gpu-viewer*.deb
 #micro
 wget -q --show-progress "$(wget -qO- https://api.github.com/repos/zyedidia/micro/releases|grep browser_download_url|grep amd64.deb|head -n1|cut -d '"' -f4)"
 dpkg-name micro*.deb
+#planck
+wget -q --show-progress "$(wget -qO- https://api.github.com/repos/stella-labs/planck-releases/releases|grep browser_download_url|grep .deb|head -n1|cut -d '"' -f4)"
+dpkg-name planck*.deb
 #portmaster
 wget -q --show-progress https://updates.safing.io/latest/linux_amd64/packages/portmaster-installer.deb
 dpkg-name portmaster-installer.deb
