3c28fbe91949c60ce24ff16046760110be3aa313
Raul Dipeas <github.rauldipeas@lock.email>
2024-11-23 21:59:02 -0300

    +unknown-wallpapers

--- scripts/wallpapers.sh
+++ scripts/wallpapers.sh
@@ -123,3 +123,55 @@ cat <<EOF |tee /tmp/respin-rdx/config/includes.chroot/usr/share/gnome-background
 	</wallpaper>
 </wallpapers>
 EOF
+cp -r assets/unknown /tmp/respin-rdx/config/includes.chroot/usr/share/backgrounds/unknown
+cat <<EOF |tee /tmp/respin-rdx/config/includes.chroot/usr/share/gnome-background-properties/unknown.xml>/dev/null
+<?xml version="1.0"?>
+<!DOCTYPE wallpapers SYSTEM "gnome-wp-list.dtd">
+<wallpapers>
+	<wallpaper deleted="false">
+		<name>Unknown 1</name>
+		<filename>/usr/share/backgrounds/unknown/729666.jpg</filename>
+		<options>zoom</options>
+	</wallpaper>
+	<wallpaper deleted="false">
+		<name>Unknown 1</name>
+		<filename>/usr/share/backgrounds/unknown/abstract-blue-wave-background-digital-art-4k-wallpaper-uhdpaper.com-486@0@f.jpg</filename>
+		<options>zoom</options>
+	</wallpaper>
+	<wallpaper deleted="false">
+		<name>Unknown 1</name>
+		<filename>/usr/share/backgrounds/unknown/abstract-colorful-wave-3-4k.jpg</filename>
+		<options>zoom</options>
+	</wallpaper>
+	<wallpaper deleted="false">
+		<name>Unknown 1</name>
+		<filename>/usr/share/backgrounds/unknown/abstract-digital-art-uhdpaper.com-4K-4.301.jpg</filename>
+		<options>zoom</options>
+	</wallpaper>
+	<wallpaper deleted="false">
+		<name>Unknown 1</name>
+		<filename>/usr/share/backgrounds/unknown/abstract-illustration-colorful-art-15-4k.jpg</filename>
+		<options>zoom</options>
+	</wallpaper>
+	<wallpaper deleted="false">
+		<name>Unknown 1</name>
+		<filename>/usr/share/backgrounds/unknown/backiee-292330-landscape.jpg</filename>
+		<options>zoom</options>
+	</wallpaper>
+	<wallpaper deleted="false">
+		<name>Unknown 1</name>
+		<filename>/usr/share/backgrounds/unknown/Waves-Dark-6016x6016.jpg</filename>
+		<options>zoom</options>
+	</wallpaper>
+	<wallpaper deleted="false">
+		<name>Unknown 1</name>
+		<filename>/usr/share/backgrounds/unknown/Waves-Dark-Alt-6016x6016.jpg</filename>
+		<options>zoom</options>
+	</wallpaper>
+	<wallpaper deleted="false">
+		<name>Unknown 1</name>
+		<filename>/usr/share/backgrounds/unknown/Waves-Light-6016x6016.jpg</filename>
+		<options>zoom</options>
+	</wallpaper>
+</wallpapers>
+EOF
