ce25a54178136a8168dadc2c9492444f7083f92b
Raul Dipeas <github.rauldipeas@lock.email>
2024-12-02 22:44:49 -0300

    ^gtk-fix ^iriun ^rclone

--- scripts/hooks/apt/iriun.sh
+++ scripts/hooks/apt/iriun.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 apt install -y -t bookworm-backports iriunwebcam
-sed -i 's|Exec=|Exec=GTK2_RC_FILE=/usr/share/themes/Orchis-Dark-Compact/gtk-2.0/gtkrc|' /usr/share/applications/iriunwebcam.desktop
+sed -i "s|Exec=|Exec=bash -c 'GTK2_RC_FILES=/usr/share/themes/Orchis-Dark-Compact/gtk-2.0/gtkrc |'" /usr/share/applications/iriunwebcam.desktop
