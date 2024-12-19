88e06a6299fd487a4f52cd74cb9289293f7451a2
Raul Dipeas <github.rauldipeas@lock.email>
2024-12-19 03:35:34 -0300

    ^gpu-driver

--- scripts/hooks/apt/calamares.d/gpu-driver.bash
+++ scripts/hooks/apt/calamares.d/gpu-driver.bash
@@ -1,18 +1,19 @@
 #!/bin/bash
 set -e
-if [ "$(cut -d' ' -f9 <(grep NVIDIA <(sudo lshw -C display)))" == NVIDIA ];then
-    sudo apt autoremove --purge -y "*nvidia*"
-    sudo apt install -y -t bookworm-backports firmware-misc-nonfree nvidia-driver
-    echo 'NVIDIA'|sudo tee /home/"$(ls /home)"/.gpu-driver>/dev/null
-elif [ "$(cut -d' ' -f9 <(grep AMD <(sudo lshw -C display)))" == AMD ];then
-    echo 'AMD'|sudo tee /home/"$(ls /home)"/.gpu-driver>/dev/null
-elif [ "$(cut -d' ' -f9 <(grep Intel <(sudo lshw -C display)))" == Intel ];then
-    echo 'Intel'|sudo tee /home/"$(ls /home)"/.gpu-driver>/dev/null
-elif [ "$(cut -d' ' -f9 <(grep VirtualBox <(sudo lshw -C display)))" == VirtualBox ];then
-    echo "deb http://fasttrack.debian.net/debian-fasttrack/ $(lsb_release -cs)-fasttrack main contrib"|sudo tee /etc/apt/sources.list.d/fasttrack.list>/dev/null
-    echo "deb http://fasttrack.debian.net/debian-fasttrack/ $(lsb_release -cs)-backports-staging main contrib"|sudo tee -a /etc/apt/sources.list.d/fasttrack.list>/dev/null
-    sudo apt install -y fasttrack-archive-keyring
-    sudo apt update
-    sudo apt install --no-install-recommends -y virtualbox-guest-x11
-    echo 'VirtualBox'|sudo tee /home/"$(ls /home)"/.gpu-driver>/dev/null
+if [ "$(cut -d' ' -f9 <(grep NVIDIA <(lshw -C display)))" == NVIDIA ];then
+    apt autoremove --purge -y "*nvidia*"
+    apt install -y nvidia-detect
+    apt install -y -t bookworm-backports firmware-misc-nonfree "$(nvidia-detect|grep nvidia-|cut -d ' ' -f5)"
+    echo 'NVIDIA'|tee /home/"$(ls /home)"/.gpu-driver>/dev/null
+elif [ "$(cut -d' ' -f9 <(grep AMD <(lshw -C display)))" == AMD ];then
+    echo 'AMD'|tee /home/"$(ls /home)"/.gpu-driver>/dev/null
+elif [ "$(cut -d' ' -f9 <(grep Intel <(lshw -C display)))" == Intel ];then
+    echo 'Intel'|tee /home/"$(ls /home)"/.gpu-driver>/dev/null
+elif [ "$(cut -d' ' -f9 <(grep VirtualBox <(lshw -C display)))" == VirtualBox ];then
+    echo "deb http://fasttrack.debian.net/debian-fasttrack/ $(lsb_release -cs)-fasttrack main contrib"|tee /etc/apt/sources.list.d/fasttrack.list>/dev/null
+    echo "deb http://fasttrack.debian.net/debian-fasttrack/ $(lsb_release -cs)-backports-staging main contrib"|tee -a /etc/apt/sources.list.d/fasttrack.list>/dev/null
+    apt install -y fasttrack-archive-keyring
+    apt update
+    apt install --no-install-recommends -y virtualbox-guest-x11
+    echo 'VirtualBox'|tee /home/"$(ls /home)"/.gpu-driver>/dev/null
 fi
