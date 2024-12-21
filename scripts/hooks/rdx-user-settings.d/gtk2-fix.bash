58b0eade108d86efc76cd61158b07286bae1321c
Raul Dipeas <github.rauldipeas@lock.email>
2024-12-21 02:28:10 -0300

    ^gtk2-fix

--- scripts/hooks/rdx-user-settings.d/gtk2-fix.bash
+++ scripts/hooks/rdx-user-settings.d/gtk2-fix.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-if "$(cat "$@"|grep GTK2_RC_FILES)";then
+if grep -q GTK2_RC_FILES "$@";then
     echo 'Already fixed'
     else
     sudo sd 'Exec=' "Exec=bash -c 'GTK2_RC_FILES=/usr/share/themes/Orchis-Dark-Compact/gtk-2.0/gtkrc '" "$@"
