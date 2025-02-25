0d5f755191d45088d7ca10221396cab330548d32
Raul Dipeas <github.rauldipeas@lock.email>
2025-02-25 02:55:40 -0300

    ^site

---
+++ site/assets/js/kofi.js
@@ -0,0 +1,6 @@
+kofiWidgetOverlay.draw('rauldipeas', {
+    'type': 'floating-chat',
+    'floating-chat.donateButton.text': 'Donate',
+    'floating-chat.donateButton.background-color': '#794bc4',
+    'floating-chat.donateButton.text-color': '#fff'
+});
