fea9226e5933a6ee879ab6ebfd4bab9d36557e3c
Raul Dipeas <github.rauldipeas@lock.email>
2025-03-08 03:56:21 -0300

    ^am

--- scripts/hooks/am.sh
+++ scripts/hooks/am.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 wget -qO- https://github.com/ivan-hc/AM/raw/main/INSTALL|sudo bash -
-wget -qO /opt/am/APP-MANAGER https://github.com/ivan-hc/AM/raw/snapshot-20250305/APP-MANAGER
+#wget -qO /opt/am/APP-MANAGER https://github.com/ivan-hc/AM/raw/snapshot-20250305/APP-MANAGER
