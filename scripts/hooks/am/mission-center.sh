5e2d9392f02dff984c610d19d2aa5c0ce88a895a
Raul Dipeas <github.rauldipeas@lock.email>
2025-03-09 19:39:04 -0300

    ^mission-center ^update

--- scripts/hooks/am/mission-center.sh
+++ scripts/hooks/am/mission-center.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 am -i --icons mission-center
-sed -i 's|Icon=missioncenter|Icon=io.missioncenter.MissionCenter|' /usr/local/share/applications/mission-center-AM.desktop
+sed -i 's|Icon=mission-center|Icon=io.missioncenter.MissionCenter|' /usr/local/share/applications/mission-center-AM.desktop
 cat <<EOF |tee -a /usr/local/share/applications/mission-center-AM.desktop
 StartupWMClass=missioncenter
 EOF
--- updates/update.sh
+++ updates/update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+
 # Alpha 0.2
 sudo nala install -y -t bookworm-backports\
     linux-headers-rt\
@@ -7,3 +8,11 @@ sudo nala install -y -t bookworm-backports\
 sudo nala install -y\
     rtirq-init
 am -i cryptomator
+
+# 09/03/2025 | 19:38
+am -r mission-center
+am -i --icons mission-center
+sed -i 's|Icon=mission-center|Icon=io.missioncenter.MissionCenter|' /usr/local/share/applications/mission-center-AM.desktop
+cat <<EOF |tee -a /usr/local/share/applications/mission-center-AM.desktop
+StartupWMClass=missioncenter
+EOF
