5cc89899ce6bca21f3b1625ec90361490174d4e6
Raul Dipeas <github.rauldipeas@lock.email>
2025-03-09 16:24:25 -0300

    ^mission-center ^site

--- scripts/hooks/am/mission-center.sh
+++ scripts/hooks/am/mission-center.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 set -e
-am -i mission-center
+am -i --icons mission-center
+sed -i 's|Icon=missioncenter|Icon=io.missioncenter.MissionCenter|' /usr/local/share/applications/mission-center-AM.desktop
+cat <<EOF |tee -a /usr/local/share/applications/mission-center-AM.desktop
+StartupWMClass=missioncenter
+EOF
