04cb42530cbb1fec91126b3ab790a3b7260c4515
Raul Dipeas <github.rauldipeas@lock.email>
2025-04-24 23:35:19 -0300

    ^genymotion

--- scripts/hooks/genymotion.sh
+++ scripts/hooks/genymotion.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
-wget -q --show-progress -O /tmp/genymotion.bin "$(wget -qO- https://www.genymotion.com/download|grep linux_x64.bin|head -n 1|cut -d '"' -f4)"
-chmod +x /tmp/genymotion.bin
-/tmp/genymotion.bin -y
-rm /tmp/genymotion.bin
+wget -q --show-progress -O /tmp/genymotion.run "$(wget -qO- https://www.genymotion.com/download|grep linux_x64.run|head -n 1|cut -d '"' -f4)"
+chmod +x /tmp/genymotion.run
+/tmp/genymotion.run -y
+rm /tmp/genymotion.run
 sed -i 's|Icon=|#Icon=|' /usr/local/share/applications/genymobile-genymotion.desktop
 cat <<EOF |tee -a /usr/local/share/applications/genymobile-genymotion.desktop
 Icon=genymotion
