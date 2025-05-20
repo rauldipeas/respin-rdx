b5c37e4fd9389b6cda50e6c64b74b10bbb7ad427
Raul Dipeas <github.rauldipeas@lock.email>
2025-05-20 01:31:17 -0300

    ^env

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
-wget -q --show-progress http://ftp.us.debian.org/debian/pool/main/d/debian-archive-keyring/debian-archive-keyring_2023.3+deb12u2_all.deb
+#wget -q --show-progress http://ftp.us.debian.org/debian/pool/main/d/debian-archive-keyring/debian-archive-keyring_2023.3+deb12u2_all.deb
+export DEBIAN_FRONTEND='noninteractive'
+echo 'deb http://archive.debian.org/debian/ bookworm main'|tee /etc/apt/sources.list.d/bookworm.list
+apt update -o Acquire::AllowInsecureRepositories=true -o Acquire::AllowDowngradeToInsecureRepositories=true -o APT::Get::AllowUnauthenticated=true
+apt install -y --no-install-recommends -o APT::Get::AllowUnauthenticated=true debian-archive-keyring
+rm /etc/apt/sources.list.d/bookworm.list
+apt update
 wget -q --show-progress http://ftp.us.debian.org/debian/pool/main/l/live-build/live-build_20230502_all.deb
-sudo apt update
-sudo apt install -y ./debian-archive-keyring*.deb ./live-build*.deb
+apt install -y ./live-build*.deb
 mkdir -p /tmp/respin-rdx
