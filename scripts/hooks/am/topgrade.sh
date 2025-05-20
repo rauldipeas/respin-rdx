167fbac7f9a5b27bc53b206ff9839fd295e78957
Raul Dipeas <github.rauldipeas@lock.email>
2025-05-19 23:14:39 -0300

    ^topgrade

--- scripts/hooks/am/topgrade.sh
+++ scripts/hooks/am/topgrade.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 am -i topgrade
-wget -qO /opt/topgrade/topgrade https://github.com/SteveLauC/topgrade/releases/download/v16.0.3_glibc_2.36/topgrade #tmp-fix
+#wget -qO /opt/topgrade/topgrade https://github.com/SteveLauC/topgrade/releases/download/v16.0.3_glibc_2.36/topgrade #tmp-fix
